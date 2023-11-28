0c743263303e7bec881255820dabcb90b8e12f71
Tom Mrazauskas <tom@mrazauskas.de>
2023-11-28 10:19:54 +0200

    draft example

---
+++ examples/toHaveProperty.test.ts
@@ -0,0 +1,20 @@
+import { expect } from "tstyche";
+
+type Worker<T> = {
+  [K in keyof T as Exclude<K, "setup" | "teardown">]: T[K];
+};
+
+interface Sample {
+  isBusy?: boolean | undefined;
+  runTest: (a: string, b: number) => void;
+  setup: () => void;
+  teardown: () => void;
+}
+
+expect<Worker<Sample>>().type.toHaveProperty("isBusy?");
+expect<Worker<Sample>>().type.toHaveProperty("runTest");
+
+expect<Worker<Sample>>().type.toHaveProperty("setup"); // Error: Property 'setup' does not exist on type 'X'.
+
+expect<Worker<Sample>>().type.not.toHaveProperty("setup");
+expect<Worker<Sample>>().type.not.toHaveProperty("teardown");
