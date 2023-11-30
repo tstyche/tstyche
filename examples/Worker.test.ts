844c5efec9a74fa1b2d2543bb92fb8c473c2f894
Tom Mrazauskas <tom@mrazauskas.de>
2023-11-30 08:29:12 +0200

    temp

--- examples/Worker.test.ts
+++ examples/Worker.test.ts
@@ -5,12 +5,30 @@ type Worker<T> = {
 };
 
 interface Sample {
-  isBusy?: boolean | undefined;
+  isBusy?: { a: boolean };
   runTest: (a: string, b: number) => void;
   setup: () => void;
 }
 
-expect<Worker<Sample>>().type.toHaveProperty("isBusy?");
+declare const workerSample: Worker<Sample>;
+
+expect<Worker<Sample>>().type.toHaveOptionalProperty("isBusy");
+expect(workerSample).type.toHaveOptionalProperty("isBusy");
+expect(workerSample.isBusy?.a).type.toEqual<boolean | undefined>();
+
 expect<Worker<Sample>>().type.toHaveProperty("runTest");
 
 expect<Worker<Sample>>().type.not.toHaveProperty("setup");
+
+workerSample.isFirtst;
+
+const kOne = Symbol("one");
+// const kTwo = Symbol();
+
+const sample = {
+  [kOne]: "one",
+  ["key"]: "test",
+};
+
+expect.skip(sample).type.toHaveProperty("key");
+// expect(sample).type.toHaveProperty(kTwo);
