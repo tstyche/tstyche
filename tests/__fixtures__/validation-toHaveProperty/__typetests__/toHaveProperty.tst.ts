343d51589054185313146a00cde2a0dc4e201fba
Tom Mrazauskas <tom@mrazauskas.de>
2023-12-17 16:25:40 +0200

    fix: make argument checks stricter for `.toHaveProperty()`

--- tests/__fixtures__/validation-toHaveProperty/__typetests__/toHaveProperty.tst.ts
+++ tests/__fixtures__/validation-toHaveProperty/__typetests__/toHaveProperty.tst.ts
@@ -12,8 +12,14 @@ describe("argument for 'source'", () => {
 
 describe("type argument for 'Source'", () => {
   test("must be of an object type", () => {
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    expect<{}>().type.not.toHaveProperty("abc");
+
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     expect<any>().type.toHaveProperty("runTest");
+    expect<never>().type.toHaveProperty("runTest");
+    expect(null).type.toHaveProperty("runTest");
+    expect<"one" | "two">().type.toHaveProperty("runTest");
   });
 });
 
