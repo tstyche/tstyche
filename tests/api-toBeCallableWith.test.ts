36590f870a3413eb7449c1017efb01eaac996e2c
Tom Mrazauskas <tom@mrazauskas.de>
2023-12-21 14:02:40 +0200

    fix file naming

--- tests/api-toBeCallableWith.test.ts
+++ tests/api-toBeCallableWith.test.ts
@@ -1,8 +1,8 @@
 import { expect, test } from "@jest/globals";
 import { spawnTyche } from "./__utils__/spawnTyche.js";
 
-test("toBeCallable", () => {
-  const { status, stderr, stdout } = spawnTyche("api-toBeCallable");
+test("toBeCallableWith", () => {
+  const { status, stderr, stdout } = spawnTyche("api-toBeCallableWith");
 
   expect(stdout).toMatchSnapshot("stdout");
   expect(stderr).toMatchSnapshot("stderr");
