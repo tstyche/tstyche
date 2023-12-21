03e5eedf3229e872d3ebbaf08ec74d4623c8ae7c
Tom Mrazauskas <tom@mrazauskas.de>
2023-12-21 17:48:20 +0200

    implement 'explainArgumentCountMismatch()' method

--- src/expect/ToBeCallableWith.ts
+++ src/expect/ToBeCallableWith.ts
@@ -8,41 +8,29 @@ export class ToBeCallableWith {
     public typeChecker: TypeChecker,
   ) {}
 
-  #explain(
+  #explainArgumentCountMismatch(
     source: { node: ts.Expression | ts.TypeNode; signatures: ReadonlyArray<ts.Signature> },
-    targetNodes: Array<ts.Expression> | Array<ts.TypeNode | ts.NamedTupleMember>,
+    argumentCount: number,
     isNot: boolean,
   ) {
     const sourceText = this.compiler.isTypeNode(source.node) ? "Type expression" : "Expression";
 
-    const argumentCount = targetNodes.length;
+    const argumentCountText =
+      argumentCount === 0 ? "without arguments" : `with provided argument${argumentCount === 1 ? "" : "s"}`;
 
-    const doesCountMatch = source.signatures.some((sourceCallSignature) => {
-      const sourceParameters = sourceCallSignature.getParameters();
+    const parameterCount = source.signatures.map((sourceCallSignature) => sourceCallSignature.getParameters().length);
+    const parameterCountText = [...new Set(parameterCount)].join(", or ");
+    const parameterCountNumber = Number(parameterCountText);
 
-      return sourceParameters.length === argumentCount;
-    });
-
-    if (argumentCount === 0 || !doesCountMatch) {
-      const argumentCountText =
-        argumentCount === 0 ? "without arguments" : `with provided argument${argumentCount === 1 ? "" : "s"}`;
-
-      const parameterCount = source.signatures.map((sourceCallSignature) => sourceCallSignature.getParameters().length);
-      const parameterCountText = [...new Set(parameterCount)].join(", or ");
-
-      if (argumentCount === 0) {
-        return isNot
-          ? [Diagnostic.error(`${sourceText} can be called ${argumentCountText}.`)]
-          : [
-              Diagnostic.error([
-                `${sourceText} requires ${parameterCountText} argument${Number(parameterCountText) === 1 ? "" : "s"}.`,
-              ]),
-            ];
-      }
-    }
-
-    // TODO
-    return [];
+    return isNot
+      ? [Diagnostic.error(`${sourceText} can be called ${argumentCountText}.`)]
+      : [
+          Diagnostic.error([
+            `${sourceText} ${parameterCountNumber === 0 ? "does not take" : `requires ${parameterCountText}`} argument${
+              parameterCountNumber === 1 ? "" : "s"
+            }.`,
+          ]),
+        ];
   }
 
   match(
@@ -52,15 +40,15 @@ export class ToBeCallableWith {
   ): MatchResult {
     const argumentCount = targetNodes.length;
 
-    const isMatch = source.signatures.some((sourceCallSignature) => {
+    const isArgumentCountMatch = source.signatures.some((sourceCallSignature) => {
       const sourceParameters = sourceCallSignature.getParameters();
 
       return sourceParameters.length === argumentCount;
     });
 
     return {
-      explain: () => this.#explain(source, targetNodes, isNot),
-      isMatch,
+      explain: () => this.#explainArgumentCountMismatch(source, argumentCount, isNot),
+      isMatch: isArgumentCountMatch,
     };
   }
 }
--- tests/__fixtures__/api-toBeCallableWith/__typetests__/toBeCallableWith.test.ts
+++ tests/__fixtures__/api-toBeCallableWith/__typetests__/toBeCallableWith.test.ts
@@ -11,6 +11,16 @@ describe("when target is a value", () => {
     expect(() => null).type.not.toBeCallableWith();
   });
 
+  test("expression does not take arguments", () => {
+    expect(() => null).type.not.toBeCallableWith("one");
+    expect(() => null).type.toBeCallableWith("one");
+  });
+
+  test("expression can be called with provided argument", () => {
+    expect((a: string) => a).type.toBeCallableWith("one");
+    expect((a: string) => a).type.not.toBeCallableWith("one");
+  });
+
   test("expression requires an argument", () => {
     expect((a: string) => a).type.not.toBeCallableWith();
     expect((a: string) => a).type.toBeCallableWith();
@@ -19,6 +29,16 @@ describe("when target is a value", () => {
   test("type expression can be called without arguments", () => {
     expect<() => null>().type.toBeCallableWith();
     expect<() => null>().type.not.toBeCallableWith();
+  });
+
+  test("type expression does not take arguments", () => {
+    expect<() => null>().type.not.toBeCallableWith("one");
+    expect<() => null>().type.toBeCallableWith("one");
+  });
+
+  test("type expression can be called with provided argument", () => {
+    expect<(a: string) => string>().type.toBeCallableWith("one");
+    expect<(a: string) => string>().type.not.toBeCallableWith("one");
   });
 
   test("type expression requires an argument", () => {
@@ -33,6 +53,16 @@ describe("when target is a type", () => {
     expect(() => null).type.not.toBeCallableWith<[]>();
   });
 
+  test("expression does not take arguments", () => {
+    expect(() => null).type.not.toBeCallableWith<["one"]>();
+    expect(() => null).type.toBeCallableWith<["one"]>();
+  });
+
+  test("expression can be called with provided argument", () => {
+    expect((a: string) => a).type.toBeCallableWith<["one"]>();
+    expect((a: string) => a).type.not.toBeCallableWith<["one"]>();
+  });
+
   test("expression requires an argument", () => {
     expect((a: string) => a).type.not.toBeCallableWith<[]>();
     expect((a: string) => a).type.toBeCallableWith<[]>();
@@ -41,6 +71,16 @@ describe("when target is a type", () => {
   test("type expression can be called without arguments", () => {
     expect<() => null>().type.toBeCallableWith<[]>();
     expect<() => null>().type.not.toBeCallableWith<[]>();
+  });
+
+  test("type expression does not take arguments", () => {
+    expect<() => null>().type.not.toBeCallableWith<["one"]>();
+    expect<() => null>().type.toBeCallableWith<["one"]>();
+  });
+
+  test("type expression can be called with provided argument", () => {
+    expect<(a: string) => string>().type.toBeCallableWith<["one"]>();
+    expect<(a: string) => string>().type.not.toBeCallableWith<["one"]>();
   });
 
   test("type expression requires an argument", () => {
