b3bc02d4ff539b99c923ac634c73b81d5d73954a
Tom Mrazauskas <tom@mrazauskas.de>
2024-01-03 21:08:40 +0200

    chore: tweak benchmarking

--- benchmarks/tstyche.bench.js
+++ benchmarks/tstyche.bench.js
@@ -6,29 +6,29 @@ import { Bench } from "tinybench";
  * @param {Array<string>} [args]
  */
 function spawnTyche(args) {
-  const { error, status } = spawnSync("tstyche", args);
+  const { error, status, stdout } = spawnSync("tstyche", args, { shell: true });
 
   if (error != null) {
     throw error;
   }
 
-  return { status };
+  return { status, stdout };
 }
 
-const bench = withCodSpeed(new Bench());
+const bench = withCodSpeed(new Bench({ iterations: 8 }));
 
 bench
   .add("small run with default target", () => {
     spawnTyche(["examples"]);
   })
   .add("small run with three targets", () => {
-    spawnTyche(["examples", "--target 4.9,5.3,current"]);
+    spawnTyche(["examples", "--target 4.9,5.2,current"]);
   })
   .add("large run with default target", () => {
     spawnTyche();
   })
   .add("large run with three targets", () => {
-    spawnTyche(["--target 4.9,5.3,current"]);
+    spawnTyche(["--target 4.9,5.2,current"]);
   });
 
 await bench.run();
