3ba12e03fa94bcdb621feb04e6bf6743d9389151
Tom Mrazauskas <tom@mrazauskas.de>
2024-01-04 14:55:33 +0200

    try `bencher`

--- benchmarks/examples.bench.sh
+++ benchmarks/examples.bench.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 
-hyperfine --prepare 'tstyche --prune' --time-unit 'second' --warmup 1 'tstyche examples' 'tstyche examples --target 4.9' 'tstyche examples --target 5.2'
+targets=('tstyche examples' 'tstyche examples --target 4.9' 'tstyche examples --target 5.2')
+
+hyperfine --export-json 'examples.bench.json' --prepare 'tstyche --prune' --time-unit 'second' --warmup 1 "${targets[@]}"
