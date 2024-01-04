6a646b97b6d50c6185968c793db3d72e26f3ad40
Tom Mrazauskas <tom@mrazauskas.de>
2024-01-04 14:30:03 +0200

    color and warmup

--- benchmarks/examples.bench.sh
+++ benchmarks/examples.bench.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-hyperfine --prepare 'tstyche --prune' --time-unit 'second' 'tstyche examples' 'tstyche examples --target 4.9' 'tstyche examples --target 5.2'
+hyperfine --prepare 'tstyche --prune' --time-unit 'second' --warmup 1 'tstyche examples' 'tstyche examples --target 4.9' 'tstyche examples --target 5.2'
