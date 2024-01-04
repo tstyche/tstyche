d776e793f2a04e64e31fc2a0e20cba06400c8796
Tom Mrazauskas <tom@mrazauskas.de>
2024-01-04 18:01:56 +0200

    chore(bench): clean `npm` cache before each timing run (#79)

--- benchmarks/examples.bench.sh
+++ benchmarks/examples.bench.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
-targets=('tstyche examples' 'tstyche examples --target 4.9' 'tstyche examples --target 5.2')
-
-hyperfine --export-json 'examples.bench.json' --prepare 'tstyche --prune' --time-unit 'second' --warmup 1 "${targets[@]}"
+hyperfine \
+  --export-json 'examples.bench.json' \
+  --prepare 'tstyche --prune && npm cache clean --force' \
+  --time-unit 'second' \
+  --warmup 1 \
+  'tstyche examples' \
+  'tstyche examples --target 5.2' \
+  'tstyche examples --target 4.9'
