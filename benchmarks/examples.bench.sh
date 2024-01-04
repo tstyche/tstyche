166eb713d96e4603fed0c4e1a3eedcee1cc50e76
Tom Mrazauskas <tom@mrazauskas.de>
2024-01-04 17:53:45 +0200

    warmup

--- benchmarks/examples.bench.sh
+++ benchmarks/examples.bench.sh
@@ -4,6 +4,7 @@ hyperfine \
   --export-json 'examples.bench.json' \
   --prepare 'tstyche --prune && npm cache clean --force' \
   --time-unit 'second' \
+  --warmup 1 \
   'tstyche examples' \
   'tstyche examples --target 5.2' \
   'tstyche examples --target 4.9'
