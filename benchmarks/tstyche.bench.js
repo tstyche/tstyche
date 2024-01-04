0ea5331b58c11f5bf3cf7d7d57bd53ca442e4b7d
Tom Mrazauskas <tom@mrazauskas.de>
2024-01-04 13:23:12 +0200

    improve

--- benchmarks/tstyche.bench.js
+++ benchmarks/tstyche.bench.js
@@ -1,34 +1,21 @@
-import { spawnSync } from "node:child_process";
 import { withCodSpeed } from "@codspeed/tinybench-plugin";
 import { Bench } from "tinybench";
-
-/**
- * @param {Array<string>} [args]
- */
-function spawnTyche(args) {
-  const { error, status, stdout } = spawnSync("tstyche", args, { shell: true });
-
-  if (error != null) {
-    throw error;
-  }
-
-  return { status, stdout };
-}
+import { Cli } from "tstyche/tstyche";
 
 const bench = withCodSpeed(new Bench({ iterations: 8 }));
 
 bench
-  .add("small run with default target", () => {
-    spawnTyche(["examples"]);
+  .add("run examples with default target", async () => {
+    const cli = new Cli(process);
+    await cli.run(["examples"]);
   })
-  .add("small run with three targets", () => {
-    spawnTyche(["examples", "--target 4.9,5.2,current"]);
+  .add("run examples using TypeScript 4.9", async () => {
+    const cli = new Cli(process);
+    await cli.run(["examples", "--target", "4.9"]);
   })
-  .add("large run with default target", () => {
-    spawnTyche();
-  })
-  .add("large run with three targets", () => {
-    spawnTyche(["--target 4.9,5.2,current"]);
+  .add("run examples using TypeScript 5.2", async () => {
+    const cli = new Cli(process);
+    await cli.run(["examples", "--target", "5.2"]);
   });
 
 await bench.run();
