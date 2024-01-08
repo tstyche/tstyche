7c6cb90b74b17ab058d5d69716ca26420bb852aa
Tom Mrazauskas <tom@mrazauskas.de>
2024-01-08 10:12:39 +0200

    chore: add test for the `storePath` option

---
+++ tests/config-storePath.test.ts
@@ -0,0 +1,47 @@
+import { afterEach, describe, expect, test } from "@jest/globals";
+import { existsSync } from "fs";
+import { clearFixture, writeFixture } from "./__utils__/fixtureFactory.js";
+import { getFixtureUrl } from "./__utils__/getFixtureUrl.js";
+import { spawnTyche } from "./__utils__/spawnTyche.js";
+
+const isStringTestText = `import { expect, test } from "tstyche";
+test("is string?", () => {
+  expect<string>().type.toBeString();
+});
+`;
+
+const tsconfig = {
+  extends: "../tsconfig.json",
+  include: ["./"],
+};
+
+const fixture = "config-storePath";
+
+afterEach(async () => {
+  await clearFixture(fixture);
+});
+
+describe("'TSTYCHE_STORE_PATH' environment variable", () => {
+  test("uses provided path", async () => {
+    const storeUrl = new URL("./dummy-store", getFixtureUrl(fixture));
+
+    await writeFixture(fixture, {
+      ["__typetests__/dummy.test.ts"]: isStringTestText,
+      ["tsconfig.json"]: JSON.stringify(tsconfig, null, 2),
+    });
+
+    expect(existsSync(storeUrl)).toBe(false);
+
+    const { status, stderr, stdout } = spawnTyche(fixture, ["--install", "--target", "5.2.2"], {
+      ["TSTYCHE_STORE_PATH"]: "./dummy-store",
+    });
+
+    expect(existsSync(storeUrl)).toBe(true);
+
+    expect(stdout).toMatch(/^adds TypeScript 5.2.2 to /);
+    expect(stdout).toMatch(/dummy-store\/5.2.2\r\n$/);
+    expect(stderr).toBe("");
+
+    expect(status).toBe(0);
+  });
+});
