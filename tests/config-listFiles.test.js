f48f6c7594f22e53e4359f3ab5d921bdc3831490
Tom Mrazauskas <tom@mrazauskas.de>
2024-01-21 13:32:00 +0200

    more tests

---
+++ tests/config-listFiles.test.js
@@ -0,0 +1,81 @@
+import { afterEach, describe, expect, test } from "@jest/globals";
+import { clearFixture, writeFixture } from "./__utils__/fixtureFactory.js";
+import { normalizeOutput } from "./__utils__/normalizeOutput.js";
+import { spawnTyche } from "./__utils__/spawnTyche.js";
+
+const isStringTestText = `import { expect, test } from "tstyche";
+test("is string?", () => {
+  expect<string>().type.toBeString();
+});
+`;
+
+const isNumberTestText = `import { expect, test } from "tstyche";
+test("is number?", () => {
+  expect<number>().type.toBeNumber();
+});
+`;
+
+const fixture = "config-listFiles";
+
+afterEach(async () => {
+  await clearFixture(fixture);
+});
+
+describe("'--listFiles' command line option", () => {
+  test("lists test files and exist", async () => {
+    await writeFixture(fixture, {
+      ["__typetests__/isNumber.tst.ts"]: isNumberTestText,
+      ["__typetests__/isString.tst.ts"]: isStringTestText,
+    });
+
+    const { exitCode, stderr, stdout } = await spawnTyche(fixture, ["--listFiles"]);
+
+    expect(normalizeOutput(stdout)).toMatchSnapshot("stdout");
+    expect(normalizeOutput(stderr)).toBe("");
+
+    expect(exitCode).toBe(0);
+  });
+
+  test("when search string is provided before the option", async () => {
+    await writeFixture(fixture, {
+      ["__typetests__/isNumber.tst.ts"]: isNumberTestText,
+      ["__typetests__/isString.tst.ts"]: isStringTestText,
+    });
+
+    const { exitCode, stderr, stdout } = await spawnTyche(fixture, ["isNumber", "--listFiles"]);
+
+    expect(normalizeOutput(stdout)).toMatchSnapshot("stdout");
+    expect(normalizeOutput(stderr)).toBe("");
+
+    expect(exitCode).toBe(0);
+  });
+
+  test("when search string is provided after the option", async () => {
+    await writeFixture(fixture, {
+      ["__typetests__/isNumber.tst.ts"]: isNumberTestText,
+      ["__typetests__/isString.tst.ts"]: isStringTestText,
+    });
+
+    const { exitCode, stderr, stdout } = await spawnTyche(fixture, ["--listFiles", "isString"]);
+
+    expect(normalizeOutput(stdout)).toMatchSnapshot("stdout");
+    expect(normalizeOutput(stderr)).toBe("");
+
+    expect(exitCode).toBe(0);
+  });
+
+  test("when 'disableTestFileLookup: true' is set, does nothing", async () => {
+    await writeFixture(fixture, {
+      ["__typetests__/isNumber.tst.ts"]: isNumberTestText,
+      ["__typetests__/isString.tst.ts"]: isStringTestText,
+      ["tstyche.config.json"]: JSON.stringify({ disableTestFileLookup: true }, null, 2),
+    });
+
+    const { exitCode, stderr, stdout } = await spawnTyche(fixture, ["--listFiles"]);
+
+    expect(normalizeOutput(stdout)).toMatchSnapshot("stdout");
+    expect(normalizeOutput(stderr)).toMatchSnapshot("stderr");
+
+    expect(exitCode).toBe(0);
+  });
+});
