98e52ed455f6d0e13bcb1f6a4ec235cb79c14c0e
Tom Mrazauskas <tom@mrazauskas.de>
2024-01-23 12:08:36 +0200

    feat(config): only select test files with supported extensions (#105)

--- tests/feature-runner.test.js
+++ tests/feature-runner.test.js
@@ -3,10 +3,17 @@ import { clearFixture, writeFixture } from "./__utils__/fixtureFactory.js";
 import { normalizeOutput } from "./__utils__/normalizeOutput.js";
 import { spawnTyche } from "./__utils__/spawnTyche.js";
 
-const tsconfig = {
-  extends: "../tsconfig.json",
-  include: ["**/*"],
-};
+const isStringTestText = `import { expect, test } from "tstyche";
+test("is string?", () => {
+  expect<string>().type.toBeString();
+});
+`;
+
+const isNumberTestText = `import { expect, test } from "tstyche";
+test("is number?", () => {
+  expect<number>().type.toBeNumber();
+});
+`;
 
 const fixture = "feature-runner";
 
@@ -14,11 +21,127 @@ afterEach(async () => {
   await clearFixture(fixture);
 });
 
+describe("search strings", () => {
+  test("when search string is not provided, selects files in 'typetests' directories", async () => {
+    await writeFixture(fixture, {
+      ["__typetests__/isNumber.test.ts"]: isNumberTestText,
+      ["__typetests__/isString.test.ts"]: isStringTestText,
+      ["feature/__typetests__/isNumber.test.ts"]: isNumberTestText,
+      ["feature/__typetests__/isString.test.ts"]: isStringTestText,
+      ["feature/typetests/isNumber.test.ts"]: isNumberTestText,
+      ["feature/typetests/isString.test.ts"]: isStringTestText,
+      ["typetests/isNumber.test.ts"]: isNumberTestText,
+      ["typetests/isString.test.ts"]: isStringTestText,
+    });
+
+    const { exitCode, stderr, stdout } = await spawnTyche(fixture);
+
+    expect(normalizeOutput(stdout)).toMatchSnapshot("stdout");
+    expect(stderr).toBe("");
+
+    expect(exitCode).toBe(0);
+  });
+
+  test("when search string is not provided, selects files with '.tst.' suffix", async () => {
+    await writeFixture(fixture, {
+      ["__tests__/isNumber.tst.ts"]: isNumberTestText,
+      ["__tests__/isString.tst.ts"]: isStringTestText,
+      ["feature/__tests__/isNumber.tst.ts"]: isNumberTestText,
+      ["feature/__tests__/isString.tst.ts"]: isStringTestText,
+      ["feature/tests/isNumber.tst.ts"]: isNumberTestText,
+      ["feature/tests/isString.tst.ts"]: isStringTestText,
+      ["isNumber.tst.ts"]: isNumberTestText,
+      ["isString.tst.ts"]: isStringTestText,
+      ["tests/isNumber.tst.ts"]: isNumberTestText,
+      ["tests/isString.tst.ts"]: isStringTestText,
+    });
+
+    const { exitCode, stderr, stdout } = await spawnTyche(fixture);
+
+    expect(normalizeOutput(stdout)).toMatchSnapshot("stdout");
+    expect(stderr).toBe("");
+
+    expect(exitCode).toBe(0);
+  });
+
+  test("when single search string is provided, selects matching files", async () => {
+    await writeFixture(fixture, {
+      ["__typetests__/isNumber.test.ts"]: isNumberTestText,
+      ["__typetests__/isString.test.ts"]: isStringTestText,
+      ["feature/__tests__/isNumber.tst.ts"]: isNumberTestText,
+      ["feature/__tests__/isString.tst.ts"]: isStringTestText,
+    });
+
+    const { exitCode, stderr, stdout } = await spawnTyche(fixture, ["number"]);
+
+    expect(normalizeOutput(stdout)).toMatchSnapshot("stdout");
+    expect(stderr).toBe("");
+
+    expect(exitCode).toBe(0);
+  });
+
+  test("when multiple search strings are provided, selects matching files", async () => {
+    await writeFixture(fixture, {
+      ["__typetests__/isNumber.test.ts"]: isNumberTestText,
+      ["__typetests__/isString.test.ts"]: isStringTestText,
+      ["feature/__tests__/isNumber.tst.ts"]: isNumberTestText,
+      ["feature/__tests__/isString.tst.ts"]: isStringTestText,
+    });
+
+    const { exitCode, stderr, stdout } = await spawnTyche(fixture, ["string", "feature"]);
+
+    expect(normalizeOutput(stdout)).toMatchSnapshot("stdout");
+    expect(stderr).toBe("");
+
+    expect(exitCode).toBe(0);
+  });
+
+  test("when relative search string is provided, selects matching files", async () => {
+    await writeFixture(fixture, {
+      ["__typetests__/isNumber.test.ts"]: isNumberTestText,
+      ["__typetests__/isString.test.ts"]: isStringTestText,
+      ["feature/__tests__/isNumber.tst.ts"]: isNumberTestText,
+      ["feature/__tests__/isString.tst.ts"]: isStringTestText,
+    });
+
+    const { exitCode, stderr, stdout } = await spawnTyche(fixture, ["./feature/__tests__/isNumber"]);
+
+    expect(normalizeOutput(stdout)).toMatchSnapshot("stdout");
+    expect(stderr).toBe("");
+
+    expect(exitCode).toBe(0);
+  });
+
+  test("only the files matched by the 'testFileMatch' patterns are selected", async () => {
+    const config = {
+      testFileMatch: ["**/feature/__tests__/**.*"],
+    };
+    const tsconfig = { extends: "../../../tsconfig.json", include: ["**/*"] };
+
+    await writeFixture(fixture, {
+      ["__typetests__/isNumber.tst.ts"]: isNumberTestText,
+      ["__typetests__/isString.tst.ts"]: isStringTestText,
+      ["feature/__tests__/__snapshot__/isNumber.test.ts.snap"]: "isNumberSnap",
+      ["feature/__tests__/__snapshot__/isString.test.ts.snap"]: "isStringSnap",
+      ["feature/__tests__/isNumber.test.ts"]: isNumberTestText,
+      ["feature/__tests__/isString.test.ts"]: isStringTestText,
+      ["feature/__tests__/tsconfig.json"]: JSON.stringify(tsconfig, null, 2),
+      ["tstyche.config.json"]: JSON.stringify(config, null, 2),
+    });
+
+    const { exitCode, stderr, stdout } = await spawnTyche(fixture, ["number"]);
+
+    expect(normalizeOutput(stdout)).toMatchSnapshot("stdout");
+    expect(stderr).toBe("");
+
+    expect(exitCode).toBe(0);
+  });
+});
+
 describe("test files", () => {
   test("allows a file to be empty", async () => {
     await writeFixture(fixture, {
       ["__typetests__/dummy.test.ts"]: "",
-      ["tsconfig.json"]: JSON.stringify(tsconfig, null, 2),
     });
 
     const { exitCode, stderr, stdout } = await spawnTyche(fixture);
@@ -40,7 +163,6 @@ describe("parent", () => {
 
     await writeFixture(fixture, {
       ["__typetests__/dummy.test.ts"]: testText,
-      ["tsconfig.json"]: JSON.stringify(tsconfig, null, 2),
     });
 
     const { exitCode, stderr, stdout } = await spawnTyche(fixture);
@@ -62,7 +184,6 @@ describe.skip("skipped describe", function () {
 
     await writeFixture(fixture, {
       ["__typetests__/dummy.test.ts"]: testText,
-      ["tsconfig.json"]: JSON.stringify(tsconfig, null, 2),
     });
 
     const { exitCode, stderr, stdout } = await spawnTyche(fixture);
@@ -81,7 +202,6 @@ describe.todo("and this one");
 
     await writeFixture(fixture, {
       ["__typetests__/dummy.test.ts"]: testText,
-      ["tsconfig.json"]: JSON.stringify(tsconfig, null, 2),
     });
 
     const { exitCode, stderr, stdout } = await spawnTyche(fixture);
@@ -101,7 +221,6 @@ test("empty test", () => {
 
     await writeFixture(fixture, {
       ["__typetests__/dummy.test.ts"]: testText,
-      ["tsconfig.json"]: JSON.stringify(tsconfig, null, 2),
     });
 
     const { exitCode, stderr, stdout } = await spawnTyche(fixture);
@@ -121,7 +240,6 @@ test.skip("is skipped?", () => {
 
     await writeFixture(fixture, {
       ["__typetests__/dummy.test.ts"]: testText,
-      ["tsconfig.json"]: JSON.stringify(tsconfig, null, 2),
     });
 
     const { exitCode, stderr, stdout } = await spawnTyche(fixture);
@@ -140,7 +258,6 @@ test.todo("and this one");
 
     await writeFixture(fixture, {
       ["__typetests__/dummy.test.ts"]: testText,
-      ["tsconfig.json"]: JSON.stringify(tsconfig, null, 2),
     });
 
     const { exitCode, stderr, stdout } = await spawnTyche(fixture);
@@ -158,7 +275,6 @@ expect<number>().type.toBeNumber();
 
     await writeFixture(fixture, {
       ["__typetests__/dummy.test.ts"]: testText,
-      ["tsconfig.json"]: JSON.stringify(tsconfig, null, 2),
     });
 
     const { exitCode, stderr, stdout } = await spawnTyche(fixture);
