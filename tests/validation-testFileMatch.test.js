de3fa4e783b75eee568b59e1944875c410b811f8
Tom Mrazauskas <tom@mrazauskas.de>
2024-01-28 16:09:47 +0200

    chore: add the `testFileMatch` option validation tests

---
+++ tests/validation-testFileMatch.test.js
@@ -0,0 +1,54 @@
+import { afterEach, describe, expect, test } from "@jest/globals";
+import { clearFixture, getFixtureUrl, writeFixture } from "./__utils__/fixtureFactory.js";
+import { normalizeOutput } from "./__utils__/normalizeOutput.js";
+import { spawnTyche } from "./__utils__/spawnTyche.js";
+
+const isStringTestText = `import { expect, test } from "tstyche";
+test("is string?", () => {
+  expect<string>().type.toBeString();
+});
+`;
+
+const fixtureUrl = getFixtureUrl("validation-target", { generated: true });
+
+afterEach(async () => {
+  await clearFixture(fixtureUrl);
+});
+
+describe("'testFileMatch' configuration file option", () => {
+  test("when option argument is not a list", async () => {
+    const config = {
+      testFileMatch: "feature",
+    };
+
+    await writeFixture(fixtureUrl, {
+      ["__typetests__/dummy.test.ts"]: isStringTestText,
+      ["tstyche.config.json"]: JSON.stringify(config, null, 2),
+    });
+
+    const { exitCode, stderr, stdout } = await spawnTyche(fixtureUrl);
+
+    expect(stdout).toBe("");
+    expect(normalizeOutput(stderr)).toMatchSnapshot("stderr");
+
+    expect(exitCode).toBe(1);
+  });
+
+  test("when item of the list is not a string", async () => {
+    const config = {
+      testFileMatch: ["examples/*", false],
+    };
+
+    await writeFixture(fixtureUrl, {
+      ["__typetests__/dummy.test.ts"]: isStringTestText,
+      ["tstyche.config.json"]: JSON.stringify(config, null, 2),
+    });
+
+    const { exitCode, stderr, stdout } = await spawnTyche(fixtureUrl);
+
+    expect(stdout).toBe("");
+    expect(normalizeOutput(stderr)).toMatchSnapshot("stderr");
+
+    expect(exitCode).toBe(1);
+  });
+});
