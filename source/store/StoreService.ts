572c73641bdd76fb8e6ada080e402f2017ba3882
Tom Mrazauskas <tom@mrazauskas.de>
2024-02-24 18:56:00 +0200

    fix(config, store): do not validated tags, if the store failed to open (#133)

--- source/store/StoreService.ts
+++ source/store/StoreService.ts
@@ -164,7 +164,7 @@ export class StoreService {
     await this.#manifestWorker.open(signal, { refresh: true });
   }
 
-  async validateTag(tag: string, signal?: AbortSignal): Promise<boolean> {
+  async validateTag(tag: string, signal?: AbortSignal): Promise<boolean | undefined> {
     if (tag === "current") {
       return Environment.typescriptPath != null;
     }
@@ -172,7 +172,7 @@ export class StoreService {
     await this.open(signal);
 
     if (!this.#manifest) {
-      return false;
+      return undefined;
     }
 
     if (
