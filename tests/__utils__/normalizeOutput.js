9c981499c553530d28a0d3f12960731f9b7999fa
Tom Mrazauskas <tom@mrazauskas.de>
2024-02-25 14:48:24 +0200

    refactor: use `String.prototype.replace()` (#137)

--- tests/__utils__/normalizeOutput.js
+++ tests/__utils__/normalizeOutput.js
@@ -1,9 +1,11 @@
+const rootPath = process.cwd().replace(/\\/g, "/");
+
 /**
  * @param {string} output
  */
 export function normalizeOutput(output) {
   return output
-    .replaceAll(process.cwd().replaceAll("\\", "/"), "<<cwd>>")
-    .replaceAll(/(Duration:\s{2})\s((?:\d\.?)+s)/g, "$1 <<timestamp>>")
-    .replaceAll(/(uses TypeScript)\s(\d\.?)+/g, "$1 <<version>>");
+    .replace(new RegExp(rootPath, "g"), "<<cwd>>")
+    .replace(/(Duration:\s{2})\s((?:\d\.?)+s)/g, "$1 <<timestamp>>")
+    .replace(/(uses TypeScript)\s(\d\.?)+/g, "$1 <<version>>");
 }
