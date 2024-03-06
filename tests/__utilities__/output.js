5af94de69e937bf4c7b424c7026591f532a2ca5a
Tom Mrazauskas <tom@mrazauskas.de>
2024-03-06 22:08:51 +0200

    chore(test): improve output normalization

--- tests/__utilities__/output.js
+++ tests/__utilities__/output.js
@@ -1,3 +1,5 @@
+import ts from "typescript";
+
 const rootPath = process.cwd().replace(/\\/g, "/");
 
 /**
@@ -7,5 +9,5 @@ export function normalizeOutput(output) {
   return output
     .replace(new RegExp(rootPath, "g"), "<<cwd>>")
     .replace(/(Duration:\s{2})\s((?:\d\.?)+s)/g, "$1 <<timestamp>>")
-    .replace(/(uses TypeScript)\s(\d\.?)+/g, "$1 <<version>>");
+    .replace(new RegExp(`TypeScript ${ts.version}`, "g"), "TypeScript <<version>>");
 }
