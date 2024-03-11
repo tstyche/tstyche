c1f4d91c979d6aaee56de0329885cddb9a4abda3
Tom Mrazauskas <tom@mrazauskas.de>
2024-03-11 12:25:17 +0200

    chore: remove `Readonly` example

--- examples/Readonly.tst.ts
+++
@@ -1,9 +0,0 @@
-import { expect, test } from "tstyche";
-
-test("Readonly", () => {
-  expect<Readonly<Array<string>>>().type.not.toMatch<Array<string>>();
-  expect<Readonly<Map<string, string>>>().type.not.toMatch<Map<string, string>>();
-  expect<Readonly<Set<string>>>().type.not.toMatch<Set<string>>();
-
-  expect<Readonly<{ a: string }>>().type.not.toMatch<{ a: string }>();
-});
