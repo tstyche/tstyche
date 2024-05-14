c1f4aedf5f9a74315d9e4a5b79413e609696b0b7
Tom Mrazauskas <tom@mrazauskas.de>
2024-05-14 15:35:59 +0300

    when test files are renamed

--- tests/feature-watch-mode.test.js
+++ tests/feature-watch-mode.test.js
@@ -63,6 +63,91 @@ if (isRecursiveWatchAvailable) {
     await clearFixture(fixtureUrl);
   });
 
+  describe("interactive input", function () {
+    const exitTestCases = [
+      {
+        key: "\u0003",
+        testCase: "exits watch mode, when 'Ctrl-C' is pressed",
+      },
+      {
+        key: "\u0004",
+        testCase: "exits watch mode, when 'Ctrl-D' is pressed",
+      },
+      {
+        key: "\u001B",
+        testCase: "exits watch mode, when 'Escape' is pressed",
+      },
+      {
+        key: "x",
+        testCase: "exits watch mode, when 'x' is pressed",
+      },
+      {
+        key: "X",
+        testCase: "exits watch mode, when 'X' is pressed",
+      },
+    ];
+
+    exitTestCases.forEach(({ key, testCase }) => {
+      test(testCase, async function () {
+        const process = new Process(fixtureUrl, ["--watch"], { env: { ["CI"]: undefined } });
+
+        await process.waitForIdle();
+        await process.write(key);
+
+        const { exitCode, stderr, stdout } = await process.waitForExit();
+
+        await assert.matchSnapshot(prettyAnsi(normalizeOutput(stdout)), {
+          fileName: `${testFileName}-exit-stdout`,
+          testFileUrl: import.meta.url,
+        });
+
+        assert.equal(stderr, "");
+        assert.equal(exitCode, 0);
+      });
+    });
+
+    const runAllTestCases = [
+      {
+        key: "\u000D",
+        testCase: "runs all tests, when 'Return' is pressed",
+      },
+      {
+        key: "\u0020",
+        testCase: "runs all tests, when 'Space' is pressed",
+      },
+      {
+        key: "a",
+        testCase: "runs all tests, when 'a' is pressed",
+      },
+      {
+        key: "A",
+        testCase: "runs all tests, when 'A' is pressed",
+      },
+    ];
+
+    runAllTestCases.forEach(({ key, testCase }) => {
+      test(testCase, async function () {
+        const process = new Process(fixtureUrl, ["--watch"], { env: { ["CI"]: undefined } });
+
+        await process.waitForIdle();
+        await process.write(key);
+
+        await process.waitForIdle();
+        await process.write("x");
+
+        const { exitCode, stderr, stdout } = await process.waitForExit();
+
+        await assert.matchSnapshot(prettyAnsi(normalizeOutput(stdout)), {
+          fileName: `${testFileName}-run-all-stdout`,
+          testFileUrl: import.meta.url,
+        });
+
+        assert.equal(stderr, "");
+        assert.equal(exitCode, 0);
+      });
+    });
+  });
+
   describe("file system", function () {
     test("when single test file is added", async function () {
       const process = new Process(fixtureUrl, ["--watch"], { env: { ["CI"]: undefined } });
@@ -173,90 +258,73 @@ if (isRecursiveWatchAvailable) {
 
       assert.equal(exitCode, 0);
     });
-  });
 
-  describe("interactive input", function () {
-    const exitTestCases = [
-      {
-        key: "\u0003",
-        testCase: "exits watch mode, when 'Ctrl-C' is pressed",
-      },
-      {
-        key: "\u0004",
-        testCase: "exits watch mode, when 'Ctrl-D' is pressed",
-      },
-      {
-        key: "\u001B",
-        testCase: "exits watch mode, when 'Escape' is pressed",
-      },
-      {
-        key: "x",
-        testCase: "exits watch mode, when 'x' is pressed",
-      },
-      {
-        key: "X",
-        testCase: "exits watch mode, when 'X' is pressed",
-      },
-    ];
+    test("when single test file is renamed", async function () {
+      const process = new Process(fixtureUrl, ["--watch"], { env: { ["CI"]: undefined } });
 
-    exitTestCases.forEach(({ key, testCase }) => {
-      test(testCase, async function () {
-        const process = new Process(fixtureUrl, ["--watch"], { env: { ["CI"]: undefined } });
+      await process.waitForIdle();
+      process.resetOutput();
 
-        await process.waitForIdle();
-        await process.write(key);
+      fs.renameSync(
+        new URL("a-feature/__typetests__/isNumber.test.ts", fixtureUrl),
+        new URL("a-feature/__typetests__/new-isNumber.test.ts", fixtureUrl),
+      );
 
-        const { exitCode, stderr, stdout } = await process.waitForExit();
+      await process.waitForIdle();
+      await process.write("a");
+      const fileRenamed = await process.waitForIdle();
 
-        await assert.matchSnapshot(prettyAnsi(normalizeOutput(stdout)), {
-          fileName: `${testFileName}-exit-stdout`,
-          testFileUrl: import.meta.url,
-        });
-
-        assert.equal(stderr, "");
-        assert.equal(exitCode, 0);
+      await assert.matchSnapshot(prettyAnsi(normalizeOutput(fileRenamed.stdout)), {
+        fileName: `${testFileName}-single-test-file-is-renamed-stdout`,
+        testFileUrl: import.meta.url,
       });
+
+      await process.write("x");
+
+      const { exitCode, stderr } = await process.waitForExit();
+
+      assert.equal(stderr, "");
+      assert.equal(exitCode, 0);
     });
 
-    const runAllTestCases = [
-      {
-        key: "\u000D",
-        testCase: "runs all tests, when 'Return' is pressed",
-      },
-      {
-        key: "\u0020",
-        testCase: "runs all tests, when 'Space' is pressed",
-      },
-      {
-        key: "a",
-        testCase: "runs all tests, when 'a' is pressed",
-      },
-      {
-        key: "A",
-        testCase: "runs all tests, when 'A' is pressed",
-      },
-    ];
+    test("when multiple test files are renamed", async function () {
+      const process = new Process(fixtureUrl, ["--watch"], { env: { ["CI"]: undefined } });
 
-    runAllTestCases.forEach(({ key, testCase }) => {
-      test(testCase, async function () {
-        const process = new Process(fixtureUrl, ["--watch"], { env: { ["CI"]: undefined } });
+      await process.waitForIdle();
+      process.resetOutput();
 
-        await process.waitForIdle();
-        await process.write(key);
+      fs.renameSync(
+        new URL("a-feature/__typetests__/isNumber.test.ts", fixtureUrl),
+        new URL("a-feature/__typetests__/new-isNumber.test.ts", fixtureUrl),
+      );
+      fs.renameSync(
+        new URL("b-feature/__typetests__/isString.test.ts", fixtureUrl),
+        new URL("b-feature/__typetests__/new-isString.test.ts", fixtureUrl),
+      );
 
-        await process.waitForIdle();
-        await process.write("x");
+      await process.waitForIdle();
+      await process.write("a");
+      const fileRenamed = await process.waitForIdle();
 
-        const { exitCode, stderr, stdout } = await process.waitForExit();
-
-        await assert.matchSnapshot(prettyAnsi(normalizeOutput(stdout)), {
-          fileName: `${testFileName}-run-all-stdout`,
-          testFileUrl: import.meta.url,
-        });
-
-        assert.equal(stderr, "");
-        assert.equal(exitCode, 0);
+      await assert.matchSnapshot(prettyAnsi(normalizeOutput(fileRenamed.stdout)), {
+        fileName: `${testFileName}-multiple-test-files-are-renamed-stdout`,
+        testFileUrl: import.meta.url,
       });
+
+      await process.write("x");
+
+      const { exitCode, stderr } = await process.waitForExit();
+
+      assert.equal(stderr, "");
+      assert.equal(exitCode, 0);
+    });
+
+    test("when single test file is removed", async function () {
+      //
+    });
+
+    test("when multiple test files are removed", async function () {
+      //
     });
   });
 } else {
