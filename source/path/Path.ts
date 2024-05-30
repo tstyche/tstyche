f66a5a8aa983700ae6a393fffabfb5ae048e7f1c
Tom Mrazauskas <tom@mrazauskas.de>
2024-05-30 10:38:47 +0300

    perf(path): use initialization block instead of condition

--- source/path/Path.ts
+++ source/path/Path.ts
@@ -1,20 +1,22 @@
 import path from "node:path";
 
 export class Path {
+  static normalizeSlashes: (filePath: string) => string;
+
+  static {
+    if (path.sep === "/") {
+      Path.normalizeSlashes = (filePath) => filePath;
+    } else {
+      Path.normalizeSlashes = (filePath) => filePath.replace(/\\/g, "/");
+    }
+  }
+
   static dirname(filePath: string): string {
     return Path.normalizeSlashes(path.dirname(filePath));
   }
 
   static join(...filePaths: Array<string>): string {
     return Path.normalizeSlashes(path.join(...filePaths));
-  }
-
-  static normalizeSlashes(filePath: string): string {
-    if (path.sep === "/") {
-      return filePath;
-    }
-
-    return filePath.replace(/\\/g, "/");
   }
 
   static relative(from: string, to: string): string {
