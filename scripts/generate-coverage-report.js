11559c138e40807a48a649896f9a28f173b4b92d
Tom Mrazauskas <tom@mrazauskas.de>
2024-06-15 12:45:26 +0300

    refactor generate script

--- scripts/generate-coverage-report.js
+++ scripts/generate-coverage-report.js
@@ -1,67 +1,16 @@
+import fs from "node:fs/promises";
+import path from "node:path";
 import process from "node:process";
 import { CoverageReport } from "monocart-coverage-reports";
 
 const isCi = Boolean(process.env["CI"]);
 
-const coverageReport = new CoverageReport({
-  all: "./source",
-
-  clean: false,
-  cleanCache: true,
-
-  entryFilter: {
-    "**/node_modules/**": false,
-
-    "**/build/bin.js": true,
-    "**/build/tstyche.js": true,
-  },
-
-  sourceFilter: {
-    "**/source/*.ts": true,
-    "**/source/*/*.ts": true,
-    "**/source/*/*.tsx": true,
-  },
-
-  outputDir: "./coverage",
-
-  reports: isCi ? ["console-summary"] : ["console-details", "v8"],
-});
-
-if (isCi) {
-  for (const platform of ["ubuntu-latest", "macOS-latest", "windows-latest"]) {
-    await coverageReport.addFromDir(`./coverage/${platform}-raw`);
-  }
-} else {
-  await coverageReport.addFromDir("./coverage/raw");
-}
-
-const coverageResults = await coverageReport.generate();
-
-if (isCi) {
-  if (coverageResults == null) {
-    throw new Error("The coverage results are required.");
-  }
-
-  const coverageReport = getCodacyCoverageReport(coverageResults);
-  await uploadCodacyCoverageReport(coverageReport);
-}
-
-function resolveCommitSha() {
-  if (process.env["COMMIT_HEAD_SHA"] != null) {
-    return process.env["COMMIT_HEAD_SHA"];
-  }
-
-  if (process.env["COMMIT_SHA"] != null) {
-    return process.env["COMMIT_SHA"];
-  }
-
-  throw new Error("The commit SHA is required.");
-}
+const outputDir = "./coverage";
 
 /**
  * @param {NonNullable<import("monocart-coverage-reports").CoverageResults>} coverageResults
  */
-function getCodacyCoverageReport(coverageResults) {
+async function writeCodacyCoverageReport(coverageResults) {
   /** @type {Array<{ filename: string, coverage: Record<string, number> }>} */
   const fileReports = [];
 
@@ -87,36 +36,51 @@ function getCodacyCoverageReport(coverageResults) {
     fileReports.push({ filename: file.sourcePath, coverage });
   }
 
-  return { fileReports };
+  const outputFilePath = path.resolve(outputDir, `codacy-coverage-report-${resolveRunnerOs()}.json`);
+
+  await fs.writeFile(outputFilePath, JSON.stringify({ fileReports }, null, 2));
+
+  console.info("Coverage report was written to:", outputFilePath);
 }
 
-/**
- * @param {Record<string, unknown>} codacyReport
- */
-async function uploadCodacyCoverageReport(codacyReport) {
-  const commitUuid = resolveCommitSha();
-  const projectToken = process.env["CODACY_PROJECT_TOKEN"];
-
-  if (projectToken == null) {
-    throw new Error("The Codacy project token is required.");
+function resolveRunnerOs() {
+  if (process.env["RUNNER_OS"] != null) {
+    return process.env["RUNNER_OS"].toLowerCase();
   }
 
-  const endpoint = `https://api.codacy.com/2.0/gh/tstyche/tstyche/commit/${commitUuid}/coverage/typescript`;
-
-  try {
-    const response = await fetch(endpoint, {
-      method: "POST",
-      headers: {
-        ["Content-Type"]: "application/json",
-        ["Accept"]: "application/json",
-        ["project-token"]: projectToken,
-      },
-      body: JSON.stringify(codacyReport),
-    });
-
-    const result = await response.json();
-    console.info("Success:", result);
-  } catch (error) {
-    console.error("Error:", error);
-  }
+  return "local";
 }
+
+const coverageReport = new CoverageReport({
+  all: "./source",
+
+  clean: true,
+  cleanCache: true,
+
+  entryFilter: {
+    "**/node_modules/**": false,
+
+    "**/build/bin.js": true,
+    "**/build/tstyche.js": true,
+  },
+
+  sourceFilter: {
+    "**/source/*.ts": true,
+    "**/source/*/*.ts": true,
+    "**/source/*/*.tsx": true,
+  },
+
+  outputDir,
+
+  reports: isCi ? ["console-summary"] : ["console-details", "v8"],
+});
+
+await coverageReport.addFromDir("./coverage/v8-data");
+
+const coverageResults = await coverageReport.generate();
+
+if (!coverageResults) {
+  throw new Error("The coverage results are required.");
+}
+
+await writeCodacyCoverageReport(coverageResults);
