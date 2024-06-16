52a79235294f55ce56e3f0cf4b06e1c296544849
Tom Mrazauskas <tom@mrazauskas.de>
2024-06-16 11:18:41 +0300

    feat(watch): wrap around sync `fs.watch` (#232)

--- source/watch/Watcher.ts
+++ source/watch/Watcher.ts
@@ -1,20 +1,18 @@
-import { existsSync } from "node:fs";
-import fs from "node:fs/promises";
+import { type FSWatcher, existsSync, watch } from "node:fs";
 import { Path } from "#path";
 
-export type WatchHandler = (filePath: string) => void | Promise<void>;
+export type WatchHandler = (filePath: string) => void;
 
 export interface WatcherOptions {
   recursive?: boolean;
 }
 
 export class Watcher {
-  #abortController = new AbortController();
   #onChanged: WatchHandler;
   #onRemoved: WatchHandler;
   #recursive: boolean | undefined;
   #targetPath: string;
-  #watcher: AsyncIterable<{ filename?: string | null }> | undefined;
+  #watcher: FSWatcher | undefined;
 
   constructor(targetPath: string, onChanged: WatchHandler, onRemoved?: WatchHandler, options?: WatcherOptions) {
     this.#targetPath = targetPath;
@@ -24,28 +22,26 @@ export class Watcher {
   }
 
   close(): void {
-    this.#abortController.abort();
+    this.#watcher?.close();
   }
 
-  async watch(): Promise<void> {
-    this.#watcher = fs.watch(this.#targetPath, { recursive: this.#recursive, signal: this.#abortController.signal });
+  watch(): Promise<void> {
+    this.#watcher = watch(this.#targetPath, { recursive: this.#recursive }, (_eventType, fileName) => {
+      if (fileName != null) {
+        const filePath = Path.resolve(this.#targetPath, fileName);
 
-    try {
-      for await (const event of this.#watcher) {
-        if (event.filename != null) {
-          const filePath = Path.resolve(this.#targetPath, event.filename);
-
-          if (existsSync(filePath)) {
-            await this.#onChanged(filePath);
-          } else {
-            await this.#onRemoved(filePath);
-          }
+        if (existsSync(filePath)) {
+          this.#onChanged(filePath);
+        } else {
+          this.#onRemoved(filePath);
         }
       }
-    } catch (error) {
-      if (error instanceof Error && error.name === "AbortError") {
-        // the watcher was aborted
-      }
-    }
+    });
+
+    return new Promise((resolve) => {
+      this.#watcher?.on("close", () => {
+        resolve();
+      });
+    });
   }
 }
