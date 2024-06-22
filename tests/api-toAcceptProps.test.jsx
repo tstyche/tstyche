f437a2377360d2760580d8c0b6a69fce6fe060d9
Tom Mrazauskas <tom@mrazauskas.de>
2024-06-22 21:02:50 +0300

    test implementation

--- tests/api-toAcceptProps.test.jsx
+++ tests/api-toAcceptProps.test.jsx
@@ -1,5 +1,5 @@
 import { describe, test } from "mocha";
-// import * as tstyche from "tstyche";
+import * as tstyche from "tstyche";
 import * as assert from "./__utilities__/assert.js";
 import { getFixtureFileUrl, getTestFileName } from "./__utilities__/fixture.js";
 import { normalizeOutput } from "./__utilities__/output.js";
@@ -8,11 +8,18 @@ import { spawnTyche } from "./__utilities__/tstyche.js";
 const testFileName = getTestFileName(import.meta.url);
 const fixtureUrl = getFixtureFileUrl(testFileName);
 
+/**
+ * @param {{ name: string; quantity: string; }} props
+ */
+function Item(props) {
+  return <li>{`${props.name}: ${props.quantity}`}</li>;
+}
+
 describe("toAcceptProps", function () {
-  // test("implementation", function () {
-  //   tstyche.expect(() => null).type.toAcceptProps();
-  //   tstyche.expect(() => null).type.not.toAcceptProps("two");
-  // });
+  test("implementation", function () {
+    tstyche.expect(Item).type.toAcceptProps({ name: "one", quantity: "2" });
+    tstyche.expect(Item).type.not.toAcceptProps();
+  });
 
   test("function components", async function () {
     const { exitCode, stderr, stdout } = await spawnTyche(fixtureUrl, ["function-components"]);
