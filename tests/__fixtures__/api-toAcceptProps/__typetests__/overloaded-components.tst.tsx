065835c3d3d378929004aab12f6333521c19ec8f
Tom Mrazauskas <tom@mrazauskas.de>
2024-06-25 12:44:07 +0300

    more overload tests

--- tests/__fixtures__/api-toAcceptProps/__typetests__/overloaded-components.tst.tsx
+++ tests/__fixtures__/api-toAcceptProps/__typetests__/overloaded-components.tst.tsx
@@ -1,25 +1,26 @@
 import type React from "react";
 import { describe, expect, test } from "tstyche";
 
-type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
-  href?: never;
+type AaProps = React.AnchorHTMLAttributes<HTMLAnchorElement> & {
+  one: string;
+  two: boolean | undefined;
 };
 
-type AnchorProps = React.AnchorHTMLAttributes<HTMLAnchorElement> & {
-  href?: string;
+type BbProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  one?: undefined;
 };
 
-interface Overload {
-  (props: ButtonProps): React.JSX.Element;
-  (props: AnchorProps): React.JSX.Element;
+interface FirstOverload {
+  (props: BbProps): React.JSX.Element;
+  (props: AaProps): React.JSX.Element;
 }
 
-function Button(props: ButtonProps): React.JSX.Element;
-function Button(props: AnchorProps): React.JSX.Element;
-function Button(props: ButtonProps | AnchorProps): React.JSX.Element {
-  const hasHref = (props: ButtonProps | AnchorProps): props is AnchorProps => "href" in props;
+function First(props: BbProps): React.JSX.Element;
+function First(props: AaProps): React.JSX.Element;
+function First(props: BbProps | AaProps): React.JSX.Element {
+  const hasOne = (props: BbProps | AaProps): props is AaProps => "one" in props;
 
-  if (hasHref(props)) {
+  if (hasOne(props)) {
     return <a {...props} />;
   }
 
@@ -28,52 +29,51 @@ function Button(props: ButtonProps | AnchorProps): React.JSX.Element {
 
 describe("when target is a function component", () => {
   test("accepts props of the given type", () => {
-    expect(Button).type.toAcceptProps();
-    expect(Button).type.not.toAcceptProps(); // fail
+    expect(First).type.toAcceptProps();
+    expect(First).type.not.toAcceptProps(); // fail
 
-    expect(Button).type.toAcceptProps({});
-    expect(Button).type.not.toAcceptProps({}); // fail
+    expect(First).type.toAcceptProps({});
+    expect(First).type.not.toAcceptProps({}); // fail
 
-    expect(Button).type.toAcceptProps({ href: "sample" });
-    expect(Button).type.not.toAcceptProps({ href: "sample" }); // fail
-  });
-
-  test.todo("property is required in props type", () => {
-    //
+    expect(First).type.toAcceptProps({ one: "sample" });
+    expect(First).type.not.toAcceptProps({ one: "sample" }); // fail
   });
 
   test("property does not exist in props type", () => {
-    expect(Button).type.not.toAcceptProps({ enable: true });
-    expect(Button).type.toAcceptProps({ enable: true }); // fail
+    expect(First).type.not.toAcceptProps({ enable: true });
+    expect(First).type.toAcceptProps({ enable: true }); // fail
   });
 
-  test.todo("property type is not assignable to prop type", () => {
-    //
+  test("property type is not assignable to prop type", () => {
+    expect(First).type.not.toAcceptProps({ one: true });
+    expect(First).type.toAcceptProps({ one: true }); // fail
   });
 });
 
 describe("when target is a type", () => {
   test("accepts props of the given type", () => {
-    expect<Overload>().type.toAcceptProps();
-    expect<Overload>().type.not.toAcceptProps(); // fail
+    expect<FirstOverload>().type.toAcceptProps();
+    expect<FirstOverload>().type.not.toAcceptProps(); // fail
 
-    expect<Overload>().type.toAcceptProps<{}>();
-    expect<Overload>().type.not.toAcceptProps<{}>(); // fail
+    expect<FirstOverload>().type.toAcceptProps<{}>();
+    expect<FirstOverload>().type.not.toAcceptProps<{}>(); // fail
 
-    expect<Overload>().type.toAcceptProps({ href: "sample" });
-    expect<Overload>().type.not.toAcceptProps({ href: "sample" });
+    expect<FirstOverload>().type.toAcceptProps<{ one: string }>();
+    expect<FirstOverload>().type.not.toAcceptProps<{ one: string }>(); // fail
   });
 
-  test.todo("property is required in props type", () => {
-    //
+  test("property is required in props type", () => {
+    expect<FirstOverload>().type.not.toAcceptProps<{ two?: boolean }>();
+    expect<FirstOverload>().type.toAcceptProps<{ two?: boolean }>(); // fail
   });
 
   test("property does not exist in props type", () => {
-    expect<Overload>().type.not.toAcceptProps({ enable: true });
-    expect<Overload>().type.toAcceptProps({ enable: true }); // fail
+    expect<FirstOverload>().type.not.toAcceptProps<{ enable: boolean }>();
+    expect<FirstOverload>().type.toAcceptProps<{ enable: boolean }>(); // fail
   });
 
-  test.todo("property type is not assignable to prop type", () => {
-    //
+  test("property type is not assignable to prop type", () => {
+    expect<FirstOverload>().type.not.toAcceptProps<{ one: boolean }>();
+    expect<FirstOverload>().type.toAcceptProps<{ one: boolean }>(); // fail
   });
 });
