5d02808c72d252aa8b3474c31743fb3bda89f701
Tom Mrazauskas <tom@mrazauskas.de>
2024-06-29 21:51:25 +0300

    fix the `.fail` flag

--- source/runner/TestTreeWorker.ts
+++ source/runner/TestTreeWorker.ts
@@ -148,7 +148,7 @@ export class TestTreeWorker {
     if (assertion.isNot ? !matchResult.isMatch : matchResult.isMatch) {
       if (runMode & RunMode.Fail) {
         const text = ["The assertion was supposed to fail, but it passed.", "Consider removing the '.fail' flag."];
-        const origin = DiagnosticOrigin.fromNode(assertion.node);
+        const origin = DiagnosticOrigin.fromNode((assertion.node.expression as ts.PropertyAccessExpression).name);
 
         EventEmitter.dispatch([
           "expect:error",
