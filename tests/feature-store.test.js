8f371bdff80e57e1fc8ea61a3936ba0b213c2c59
Tom Mrazauskas <tom@mrazauskas.de>
2024-08-03 22:11:56 +0300

    fix store test

--- tests/feature-store.test.js
+++ tests/feature-store.test.js
@@ -156,6 +156,16 @@ await describe("store", async () => {
     const storeManifest = JSON.stringify({
       $version: "2",
       lastUpdated: Date.now() - 60 * 60 * 1000, // 2 hours
+      packages: {
+        "5.0.3": {
+          integrity: "sha512-xv8mOEDnigb/tN9PSMTwSEqAnUvkoXMQlicOb0IUVDBSQCgBSaAAROUZYy2IcUy5qU6XajK5jjjO7TMWqBTKZA==",
+          tarball: "https://registry.npmjs.org/typescript/-/typescript-5.0.3.tgz",
+        },
+        "5.0.4": {
+          integrity: "sha512-cW9T5W9xY37cc+jfEnaUvX91foxtHkza3Nw3wkoF4sSlKn0MONdkdEndig/qPBWXNkmplh3NzayQzCiHM4/hqw==",
+          tarball: "https://registry.npmjs.org/typescript/-/typescript-5.0.4.tgz",
+        },
+      },
       resolutions: {},
       versions: ["5.0.2", "5.0.3", "5.0.4"],
     });
@@ -181,6 +191,16 @@ await describe("store", async () => {
     const storeManifest = JSON.stringify({
       $version: "2",
       lastUpdated: Date.now() - 2.25 * 60 * 60 * 1000, // 2 hours and 15 minutes
+      packages: {
+        "5.0.3": {
+          integrity: "sha512-xv8mOEDnigb/tN9PSMTwSEqAnUvkoXMQlicOb0IUVDBSQCgBSaAAROUZYy2IcUy5qU6XajK5jjjO7TMWqBTKZA==",
+          tarball: "https://registry.npmjs.org/typescript/-/typescript-5.0.3.tgz",
+        },
+        "5.0.4": {
+          integrity: "sha512-cW9T5W9xY37cc+jfEnaUvX91foxtHkza3Nw3wkoF4sSlKn0MONdkdEndig/qPBWXNkmplh3NzayQzCiHM4/hqw==",
+          tarball: "https://registry.npmjs.org/typescript/-/typescript-5.0.4.tgz",
+        },
+      },
       resolutions: {},
       versions: ["5.0.2", "5.0.3", "5.0.4"],
     });
