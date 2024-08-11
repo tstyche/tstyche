f928778d0819e3b7d884804a87aca7eaeaedf2dd
Tom Mrazauskas <tom@mrazauskas.de>
2024-08-11 09:02:56 +0300

    chore!: drop support for Node.js 16

--- tests/config-install.test.js
+++ tests/config-install.test.js
@@ -19,11 +19,6 @@ const testFileName = getTestFileName(import.meta.url);
 const fixtureUrl = getFixtureFileUrl(testFileName, { generated: true });
 
 await describe("'--install' command line option", async () => {
-  if (process.versions.node.startsWith("16")) {
-    // store is not supported on Node.js 16
-    return;
-  }
-
   afterEach(async () => {
     await clearFixture(fixtureUrl);
   });
