bda29efe74b818ff56caaf3f266e9daa0a7a0705
Tom Mrazauskas <tom@mrazauskas.de>
2024-08-20 20:51:35 +0300

    refactor(input): remove `InputService` constructor options (#294)

--- source/input/InputService.ts
+++ source/input/InputService.ts
@@ -2,25 +2,12 @@ import process from "node:process";
 
 export type InputHandler = (chunk: string) => void;
 
-export interface ReadStream {
-  addListener: (event: "data", handler: InputHandler) => this;
-  removeListener: (event: "data", handler: InputHandler) => this;
-  setEncoding: (encoding: "utf8") => this;
-  setRawMode?: (mode: boolean) => this;
-  unref: () => this;
-}
-
-export interface InputServiceOptions {
-  stdin?: ReadStream;
-}
-
 export class InputService {
   #onInput: InputHandler;
-  #stdin: ReadStream;
+  #stdin = process.stdin;
 
-  constructor(onInput: InputHandler, options?: InputServiceOptions) {
+  constructor(onInput: InputHandler) {
     this.#onInput = onInput;
-    this.#stdin = options?.stdin ?? process.stdin;
 
     this.#stdin.setRawMode?.(true);
     this.#stdin.setEncoding("utf8");
