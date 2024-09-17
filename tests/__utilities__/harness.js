5a1ae903521ee02807e476320bc58930449f7f15
Tom Mrazauskas <tom@mrazauskas.de>
2024-09-17 14:42:29 +0300

    concurrency

--- tests/__utilities__/harness.js
+++ tests/__utilities__/harness.js
@@ -1,15 +1,49 @@
 import process from "node:process";
-import { run } from "node:test";
+import test from "node:test";
 import reporters from "node:test/reporters";
 
-const commandLineArguments = process.argv.slice(2);
+/**
+ *
+ * @param {Array<string>} files
+ * @param {{ concurrency?: boolean, only?: boolean }} [options]
+ */
+function run(files, options) {
+  test
+    .run({ concurrency: options?.concurrency, files, only: options?.only })
+    .on("test:fail", () => {
+      process.exitCode = 1;
+    })
+    .compose(new reporters.spec())
+    .pipe(process.stdout);
+}
 
-const files = commandLineArguments.filter((arg) => !arg.startsWith("--"));
-const only = commandLineArguments.includes("--only");
+/** @type {Array<string>} */
+const flags = [];
 
-run({ files, only })
-  .on("test:fail", () => {
-    process.exitCode = 1;
-  })
-  .compose(new reporters.spec())
-  .pipe(process.stdout);
+/** @type {Array<string>} */
+const parallelFiles = [];
+
+/** @type {Array<string>} */
+const serialFiles = [];
+
+for (const arg of process.argv.slice(2)) {
+  if (arg.startsWith("--")) {
+    flags.push(arg);
+  }
+
+  if (arg.startsWith("feature-")) {
+    parallelFiles.push(arg);
+  } else {
+    serialFiles.push(arg);
+  }
+}
+
+const only = flags.includes("--only");
+
+if (parallelFiles.length > 0) {
+  run(parallelFiles, { concurrency: true, only });
+}
+
+if (serialFiles.length > 0) {
+  run(serialFiles, { only });
+}
