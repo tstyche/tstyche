60e656dac410b2816dedfcf3914087e5b6cc377a
Tom Mrazauskas <tom@mrazauskas.de>
2024-09-17 13:36:39 +0300

    add harness

---
+++ tests/__utilities__/harness.js
@@ -0,0 +1,15 @@
+import process from "node:process";
+import { run } from "node:test";
+import reporters from "node:test/reporters";
+
+const commandLineArguments = process.argv.slice(2)
+
+const files = commandLineArguments.filter((arg) => !arg.startsWith("--"));
+const only = commandLineArguments.includes("--only");
+
+run({ files, only })
+  .on("test:fail", () => {
+    process.exitCode = 1;
+  })
+  .compose(new reporters.spec())
+  .pipe(process.stdout);
