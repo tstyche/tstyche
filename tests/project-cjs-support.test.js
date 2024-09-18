b492cd68399b314f4432840f471ad1d90bf03d2b
Tom Mrazauskas <tom@mrazauskas.de>
2024-09-18 07:59:39 +0300

    chore: run more tests in parallel

--- tests/project-cjs-support.test.js
+++ tests/project-cjs-support.test.js
@@ -7,8 +7,8 @@ import { spawnTyche } from "./__utilities__/tstyche.js";
 const testFileName = getTestFileName(import.meta.url);
 const fixtureUrl = getFixtureFileUrl(testFileName);
 
-await test("supports CJS projects", async (t) => {
-  await t.test("written in CJS syntax", async () => {
+await test("CommonJS", async (t) => {
+  await t.test("supports CJS syntax", async () => {
     const { exitCode, stderr, stdout } = await spawnTyche(fixtureUrl, ["cjs-syntax"]);
 
     await assert.matchSnapshot(normalizeOutput(stdout), {
@@ -24,7 +24,7 @@ await test("supports CJS projects", async (t) => {
     assert.equal(exitCode, 0);
   });
 
-  await t.test("written in ESM syntax", async () => {
+  await t.test("supports ESM syntax", async () => {
     const { exitCode, stderr, stdout } = await spawnTyche(fixtureUrl, ["esm-syntax"]);
 
     await assert.matchSnapshot(normalizeOutput(stdout), {
