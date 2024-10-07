3bc3575d8f209886193272ac6ac692be436b7a88
Tom Mrazauskas <tom@mrazauskas.de>
2024-10-07 09:48:58 +0300

    test several keys

--- tests/__fixtures__/api-omit/__typetests__/omit.tst.ts
+++ tests/__fixtures__/api-omit/__typetests__/omit.tst.ts
@@ -1,16 +1,29 @@
 import { expect, omit, test } from "tstyche";
 
-declare function returnsObject(): { one: number; two?: string };
-declare function returnsIntersection(): { one: number; two?: string } | { one: number };
-declare function returnsUnion(): { one: number } & { two?: string };
+declare function returnsObject(): { one: number; two?: string; three?: boolean };
+declare function returnsIntersection():
+  | { one: number; two?: string; three?: boolean }
+  | { one: number; three?: boolean };
+declare function returnsUnion(): { one: number } & { two?: string; three?: boolean };
 
-test("is omitted?", () => {
-  expect(omit(returnsObject(), "one")).type.toBe<{ two?: string }>();
+test("is single key omitted?", () => {
+  expect(omit(returnsObject(), "one")).type.toBe<{ two?: string; three?: boolean }>();
+  expect(returnsObject()).type.toBe<{ two?: string; three?: boolean }>(); // fail
+
+  expect(omit(returnsIntersection(), "two")).type.toBe<{ one: number; three?: boolean }>();
+  expect(returnsIntersection()).type.toBe<{ one: number; three?: boolean }>(); // fail
+
+  expect(omit(returnsUnion(), "one")).type.toBe<{ two?: string; three?: boolean }>();
+  expect(returnsUnion()).type.toBe<{ two?: string; three?: boolean }>(); // fail
+});
+
+test("are several keys omitted?", () => {
+  expect(omit(returnsObject(), "one", "three")).type.toBe<{ two?: string }>();
   expect(returnsObject()).type.toBe<{ two?: string }>(); // fail
 
-  expect(omit(returnsIntersection(), "two")).type.toBe<{ one: number }>();
+  expect(omit(returnsIntersection(), "two", "three")).type.toBe<{ one: number }>();
   expect(returnsIntersection()).type.toBe<{ one: number }>(); // fail
 
-  expect(omit(returnsUnion(), "one")).type.toBe<{ two?: string }>();
+  expect(omit(returnsUnion(), "one", "three")).type.toBe<{ two?: string }>();
   expect(returnsUnion()).type.toBe<{ two?: string }>(); // fail
 });
--- tests/__fixtures__/api-pick/__typetests__/pick.tst.ts
+++ tests/__fixtures__/api-pick/__typetests__/pick.tst.ts
@@ -1,10 +1,12 @@
 import { expect, pick, test } from "tstyche";
 
-declare function returnsObject(): { one: number; two?: string };
-declare function returnsIntersection(): { one: number; two?: string } | { one: number };
-declare function returnsUnion(): { one: number } & { two?: string };
+declare function returnsObject(): { one: number; two?: string; three?: boolean };
+declare function returnsIntersection():
+  | { one: number; two?: string; three?: boolean }
+  | { one: number; three?: boolean };
+declare function returnsUnion(): { one: number } & { two?: string; three?: boolean };
 
-test("is picked?", () => {
+test("is single key picked?", () => {
   expect(pick(returnsObject(), "two")).type.toBe<{ two?: string }>();
   expect(returnsObject()).type.toBe<{ two?: string }>(); // fail
 
@@ -13,4 +15,15 @@ test("is picked?", () => {
 
   expect(pick(returnsUnion(), "two")).type.toBe<{ two?: string }>();
   expect(returnsUnion()).type.toBe<{ two?: string }>(); // fail
+});
+
+test("are several keys picked?", () => {
+  expect(pick(returnsObject(), "one", "three")).type.toBe<{ one: number; three?: boolean }>();
+  expect(returnsObject()).type.toBe<{ one: number; three?: boolean }>(); // fail
+
+  expect(pick(returnsIntersection(), "one", "three")).type.toBe<{ one: number; three?: boolean }>();
+  expect(returnsIntersection()).type.toBe<{ one: number; three?: boolean }>(); // fail
+
+  expect(pick(returnsUnion(), "two", "three")).type.toBe<{ two?: string; three?: boolean }>();
+  expect(returnsUnion()).type.toBe<{ two?: string; three?: boolean }>(); // fail
 });
