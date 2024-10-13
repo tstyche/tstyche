507e071bde8a03bcc58f55390bc0466fd37f3339
Tom Mrazauskas <tom@mrazauskas.de>
2024-10-13 15:02:39 +0300

    clean up

--- source/config/ConfigService.ts
+++ source/config/ConfigService.ts
@@ -79,8 +79,8 @@ export class ConfigService {
     await configFileWorker.parse();
   }
 
-  // TODO take 'userConfig' and spread it as last one.
-  //      Can be used in 'integration-Runner.test.js'
+  // TODO take 'userOptions: Partial<ResolvedConfig>' and spread it as last one.
+  //      (can be used in 'integration-Runner.test.js')
   resolveConfig(): ResolvedConfig {
     return {
       ...defaultOptions,
