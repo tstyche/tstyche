9da35b049f5d3362a3cdea4f7396ec6cdd81649b
Tom Mrazauskas <tom@mrazauskas.de>
2024-10-13 23:51:54 +0300

    format

--- tests/__fixtures__/hooks-select/select-plugin-1.js
+++ tests/__fixtures__/hooks-select/select-plugin-1.js
@@ -1,6 +1,6 @@
 /**
-  * @type {import("tstyche/tstyche").Plugin}
-  */
+ * @type {import("tstyche/tstyche").Plugin}
+ */
 export default {
   select: (testFiles) => {
     return [...testFiles, new URL("./ts-tests/toBeString.test.ts", import.meta.url)];
--- tests/__fixtures__/hooks-select/select-plugin-2.js
+++ tests/__fixtures__/hooks-select/select-plugin-2.js
@@ -1,6 +1,6 @@
 /**
-  * @type {import("tstyche/tstyche").Plugin}
-  */
+ * @type {import("tstyche/tstyche").Plugin}
+ */
 export default {
   select: () => {
     return [new URL("./ts-tests/toBeNumber.test.ts", import.meta.url)];
