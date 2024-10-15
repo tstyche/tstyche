faae9c23e9e5b748286fbdc848db4757e6793694
Tom Mrazauskas <tom@mrazauskas.de>
2024-10-15 11:35:14 +0300

    tweak test

--- tests/integration-Runner.test.js
+++ tests/integration-Runner.test.js
@@ -9,8 +9,6 @@ const isWindows = process.platform === "win32";
 const testFileName = getTestFileName(import.meta.url);
 const fixtureUrl = getFixtureFileUrl(testFileName);
 
-const resolvedConfig = tstyche.ConfigService.resolveConfig({ configFileOptions: { reporters: [] } });
-
 const eventEmitter = new tstyche.EventEmitter();
 
 /**
@@ -59,6 +57,7 @@ await test("Runner", async (t) => {
 
   for (const { testCase, testFiles } of testCases) {
     await t.test(testCase, async () => {
+      const resolvedConfig = tstyche.ConfigService.resolveConfig({ configFileOptions: { reporters: [] } });
       const runner = new tstyche.Runner(resolvedConfig);
 
       eventEmitter.addReporter(new TestResultReporter());
@@ -74,6 +73,7 @@ await test("Runner", async (t) => {
   }
 
   await t.test("when test file is a 'Task' object", async (t) => {
+    const resolvedConfig = tstyche.ConfigService.resolveConfig({ configFileOptions: { reporters: [] } });
     const runner = new tstyche.Runner(resolvedConfig);
 
     eventEmitter.addReporter(new TestResultReporter());
@@ -161,11 +161,6 @@ await test("Runner", async (t) => {
   });
 
   await t.test("configuration options", async (t) => {
-    /**
-     * @type {import("tstyche/tstyche").Runner | undefined}
-     */
-    let runner;
-
     t.beforeEach(() => {
       eventEmitter.addReporter(new TestResultReporter());
     });
@@ -175,7 +170,10 @@ await test("Runner", async (t) => {
     });
 
     await t.test("when 'failFast: true' is specified", async () => {
-      runner = new tstyche.Runner({ ...resolvedConfig, failFast: true });
+      const resolvedConfig = tstyche.ConfigService.resolveConfig({
+        configFileOptions: { reporters: [], failFast: true },
+      });
+      const runner = new tstyche.Runner(resolvedConfig);
 
       const testFiles = [
         new URL("./__typetests__/toBeNumber.tst.ts", fixtureUrl),
