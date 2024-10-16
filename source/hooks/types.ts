eafd9c1e47077e83dfa3a894fa0b00492c89be1a
Tom Mrazauskas <tom@mrazauskas.de>
2024-10-16 22:29:10 +0300

    refactor: clean up types

--- source/hooks/types.ts
+++ source/hooks/types.ts
@@ -4,9 +4,9 @@ export interface Hooks {
   /**
    * Is called after configuration is resolved and allows to modify it.
    */
-  config?: (resolvedConfig: ResolvedConfig) => ResolvedConfig | Promise<ResolvedConfig> | null | undefined;
+  config?: (resolvedConfig: ResolvedConfig) => ResolvedConfig | Promise<ResolvedConfig>;
   /**
    * Is called after test files are selected and allows to modify the list.
    */
-  select?: (testFiles: Array<string>) => Array<string | URL> | Promise<Array<string | URL>> | null | undefined;
+  select?: (testFiles: Array<string>) => Array<string | URL> | Promise<Array<string | URL>>;
 }
