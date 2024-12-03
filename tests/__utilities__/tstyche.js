ff03c36dad35e1eb601bed2658e1e3df58832e73
Tom Mrazauskas <tom@mrazauskas.de>
2024-12-03 09:28:28 +0200

    hm.. windows

--- tests/__utilities__/tstyche.js
+++ tests/__utilities__/tstyche.js
@@ -18,6 +18,7 @@ export async function spawnTyche(fixtureUrl, args, options) {
         ...options?.env,
       },
       shell: true,
+      windowsVerbatimArguments: true,
     });
 
     let stdoutOutput = "";
