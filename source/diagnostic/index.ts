56aa8b00c291acdfcea8526731f8a5e239ba61b4
Tom Mrazauskas <tom@mrazauskas.de>
2024-12-29 20:09:42 +0200

    chore: use `verbatimModuleSyntax` compiler option

--- source/diagnostic/index.ts
+++ source/diagnostic/index.ts
@@ -2,4 +2,4 @@ export { Diagnostic } from "./Diagnostic.js";
 export { DiagnosticCategory } from "./DiagnosticCategory.enum.js";
 export { DiagnosticOrigin } from "./DiagnosticOrigin.js";
 export { SourceFile } from "./SourceFile.js";
-export { DiagnosticsHandler } from "./types.js";
+export type { DiagnosticsHandler } from "./types.js";
