b80dd5c018f8ee1ceea4111ac335ae8d23761acc
mrazauskas <tom@mrazauskas.de>
2025-04-06 17:55:28 +0300

    add TODO

--- tests/api-toBeApplicable.test.js
+++ tests/api-toBeApplicable.test.js
@@ -1,5 +1,4 @@
 import test from "node:test";
-// import * as tstyche from "tstyche";
 import * as assert from "./__utilities__/assert.js";
 import { clearFixture, getFixtureFileUrl, getTestFileName, writeFixture } from "./__utilities__/fixture.js";
 import { normalizeOutput } from "./__utilities__/output.js";
@@ -10,10 +9,38 @@ const fixtureUrl = getFixtureFileUrl(testFileName);
 
 await test("toBeApplicable", async (t) => {
   await t.test("'toBeApplicable' implementation", () => {
-    // TODO
+    // TODO implement after decorator support will be added to Node.js
     //
-    // tstyche.expect({ a: "sample" }).type.toBeAssignableTo({ a: "sample" });
-    // tstyche.expect({ a: "sample" }).type.not.toBeAssignableTo({ b: "sample" });
+    // /**
+    //  * @param {unknown} _target
+    //  * @param {ClassMemberDecoratorContext & { private: false }} _context
+    //  */
+    // function publicOnly(_target, _context) {
+    //   // ..
+    // }
+    //
+    // /** @param {unknown} _target */
+    // function replaceMethod(_target) {
+    //   /** @this {_Person} */
+    //   return function () {
+    //     return `How are you, ${this.name}?`;
+    //   };
+    // }
+    //
+    // class _Person {
+    //   @(tstyche.expect(publicOnly).type.not.toBeApplicable)
+    //   #id;
+    //
+    //   /** @param {string} name */
+    //   constructor(name) {
+    //     this.#id = 0;
+    //     this.name = name;
+    //   }
+    //   @(tstyche.expect(replaceMethod).type.toBeApplicable)
+    //   hello() {
+    //     return `Hi ${this.name}!`;
+    //   }
+    // }
   });
 
   await t.test("toBeApplicable", async () => {
