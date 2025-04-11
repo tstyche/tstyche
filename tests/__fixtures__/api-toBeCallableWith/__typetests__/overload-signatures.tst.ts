7b4e6b8592555c64b792ff9e8638bc823c9ded62
mrazauskas <tom@mrazauskas.de>
2025-04-11 14:26:24 +0300

    add overload signatures test

---
+++ tests/__fixtures__/api-toBeCallableWith/__typetests__/overload-signatures.tst.ts
@@ -0,0 +1,53 @@
+import { describe, expect, test } from "tstyche";
+
+function makeDate(timestamp: number): Date;
+function makeDate(m: number, d: number, y: number): Date;
+function makeDate(mOrTimestamp: number, d?: number, y?: number): Date {
+  if (d != null && y != null) {
+    return new Date(y, mOrTimestamp, d);
+  }
+
+  return new Date(mOrTimestamp);
+}
+
+declare function t(name: string, cb: () => Promise<unknown>): Promise<void>;
+declare function t(name: string, cb: () => unknown): void;
+
+describe("when target is an expression", () => {
+  test("can be called with the given argument", () => {
+    expect(makeDate).type.toBeCallableWith(12345678);
+    expect(makeDate).type.not.toBeCallableWith(12345678); // fail
+  });
+
+  test("can be called with the given arguments", () => {
+    expect(makeDate).type.toBeCallableWith(4, 5, 6);
+    expect(makeDate).type.not.toBeCallableWith(4, 5, 6); // fail
+
+    expect(t).type.toBeCallableWith("one", () => {});
+    expect(t).type.not.toBeCallableWith("one", () => {}); // fail
+
+    expect(t).type.toBeCallableWith("two", () => Promise.resolve());
+    expect(t).type.not.toBeCallableWith("two", () => Promise.resolve()); // fail
+  });
+
+  test("cannot be called without arguments", () => {
+    expect(makeDate).type.not.toBeCallableWith();
+    expect(makeDate).type.toBeCallableWith(); // fail: Expected 1-3 arguments, but got 0.
+
+    expect(t).type.not.toBeCallableWith();
+    expect(t).type.toBeCallableWith(); // fail: Expected 2 arguments, but got 0.
+  });
+
+  test("cannot be called with the given argument", () => {
+    expect(t).type.not.toBeCallableWith("nope");
+    expect(t).type.toBeCallableWith("nope"); // fail: Expected 2 arguments, but got 1.
+  });
+
+  test("cannot be called with the given arguments", () => {
+    expect(makeDate).type.not.toBeCallableWith(2, 3);
+    expect(makeDate).type.toBeCallableWith(2, 3); // fail: No overload expects 2 arguments, but overloads do exist that expect either 1 or 3 arguments.
+
+    expect(makeDate).type.not.toBeCallableWith(4, 5, 6, 7);
+    expect(makeDate).type.toBeCallableWith(4, 5, 6, 7); // fail: Expected 1-3 arguments, but got 4.
+  });
+});
--- tests/api-toBeCallableWith.test.js
+++ tests/api-toBeCallableWith.test.js
@@ -47,4 +47,20 @@ await test("toBeCallableWith", async (t) => {
 
     assert.equal(exitCode, 1);
   });
+
+  await t.test("overload signatures", async () => {
+    const { exitCode, stderr, stdout } = await spawnTyche(fixtureUrl, ["overload-signatures.tst.ts"]);
+
+    await assert.matchSnapshot(normalizeOutput(stdout), {
+      fileName: `${testFileName}-overload-signatures-stdout`,
+      testFileUrl: import.meta.url,
+    });
+
+    await assert.matchSnapshot(stderr, {
+      fileName: `${testFileName}-overload-signatures-stderr`,
+      testFileUrl: import.meta.url,
+    });
+
+    assert.equal(exitCode, 1);
+  });
 });
