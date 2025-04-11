a8353601df555183a2322dc466edca2a1b77af93
mrazauskas <tom@mrazauskas.de>
2025-04-11 12:38:47 +0300

    add test

---
+++ tests/api-toBeCallableWith.test.js
@@ -0,0 +1,34 @@
+import test from "node:test";
+import * as tstyche from "tstyche";
+import * as assert from "./__utilities__/assert.js";
+import { getFixtureFileUrl, getTestFileName } from "./__utilities__/fixture.js";
+import { normalizeOutput } from "./__utilities__/output.js";
+import { spawnTyche } from "./__utilities__/tstyche.js";
+
+const testFileName = getTestFileName(import.meta.url);
+const fixtureUrl = getFixtureFileUrl(testFileName);
+
+const f = (/** @type {string} */ a) => a;
+
+await test("toBeCallableWith", async (t) => {
+  await t.test("'toBeCallableWith' implementation", () => {
+    tstyche.expect(f).type.toBeCallableWith("one");
+    tstyche.expect(f).type.not.toBeCallableWith(123);
+  });
+
+  await t.test("parameter arity", async () => {
+    const { exitCode, stderr, stdout } = await spawnTyche(fixtureUrl, ["parameter-arity.tst.ts"]);
+
+    await assert.matchSnapshot(normalizeOutput(stdout), {
+      fileName: `${testFileName}-parameter-arity-stdout`,
+      testFileUrl: import.meta.url,
+    });
+
+    await assert.matchSnapshot(stderr, {
+      fileName: `${testFileName}-parameter-arity-stderr`,
+      testFileUrl: import.meta.url,
+    });
+
+    assert.equal(exitCode, 1);
+  });
+});
