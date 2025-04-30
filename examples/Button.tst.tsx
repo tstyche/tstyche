0acaa9a9f576818ee0fde154b0aebd405d70b719
mrazauskas <tom@mrazauskas.de>
2025-04-30 21:45:13 +0300

    feat: add the `toAcceptChildren` matcher

--- examples/Button.tst.tsx
+++ examples/Button.tst.tsx
@@ -1,4 +1,4 @@
-import { expect, test } from "tstyche";
+import "tstyche/react";
 
 interface ButtonProps {
   text: string;
@@ -9,10 +9,30 @@ function Button({ text, type }: ButtonProps) {
   return <button type={type}>{text}</button>;
 }
 
-test("accepts props?", () => {
-  expect(Button).type.toAcceptProps({ text: "Send" });
-  expect(Button).type.toAcceptProps({ text: "Clear", type: "reset" as const });
+<tst:test name="Button">
+  <tst:expect component={Button}>
+    <tst:toAcceptProps text="Send" />
+  </tst:expect>
 
-  expect(Button).type.not.toAcceptProps({ text: "Download", type: "button" as const });
-  expect(Button).type.not.toAcceptProps({});
-});
+  <tst:expect component={Button}>
+    <tst:toAcceptProps text="Clear" type={"reset" as const} />
+  </tst:expect>
+
+  <tst:expect component={Button}>
+    <tst:not>
+      <tst:toAcceptProps text="Download" type={"button" as const} />
+    </tst:not>
+  </tst:expect>
+
+  <tst:expect component={Button}>
+    <tst:not>
+      <tst:toAcceptProps />
+    </tst:not>
+  </tst:expect>
+
+  <tst:expect component={Button}>
+    <tst:not>
+      <tst:toAcceptChildren>Nope</tst:toAcceptChildren>
+    </tst:not>
+  </tst:expect>
+</tst:test>;
