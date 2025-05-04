3dcaef254a8d0554622d0810c28e4079ea54f350
Tom Mrazauskas <tom@mrazauskas.de>
2025-05-04 07:52:56 +0300

    feat: add template test file support (#468)

---
+++ tests/api-template.test.js
@@ -0,0 +1,35 @@
+import test from "node:test";
+import * as assert from "./__utilities__/assert.js";
+import { getFixtureFileUrl, getTestFileName } from "./__utilities__/fixture.js";
+import { normalizeOutput } from "./__utilities__/output.js";
+import { spawnTyche } from "./__utilities__/tstyche.js";
+
+const testFileName = getTestFileName(import.meta.url);
+const fixtureUrl = getFixtureFileUrl(testFileName);
+
+await test("template", async (t) => {
+  // TODO figure out why this is failing on Windows
+  if (process.platform === "win32") {
+    t.skip();
+
+    return;
+  }
+
+  await t.test("template", async () => {
+    const { exitCode, stderr, stdout } = await spawnTyche(fixtureUrl, [], {
+      env: { ["NODE_OPTIONS"]: "--import ts-blank-space/register" },
+    });
+
+    await assert.matchSnapshot(normalizeOutput(stdout), {
+      fileName: `${testFileName}-stdout`,
+      testFileUrl: import.meta.url,
+    });
+
+    await assert.matchSnapshot(stderr, {
+      fileName: `${testFileName}-stderr`,
+      testFileUrl: import.meta.url,
+    });
+
+    assert.equal(exitCode, 1);
+  });
+});
