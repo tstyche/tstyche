37ad761a304e4271410b52722696451f08177786
mrazauskas <tom@mrazauskas.de>
2025-05-10 18:29:06 +0300

    refactor

--- source/collect/helpers.ts
+++ source/collect/helpers.ts
@@ -1,11 +1,7 @@
 import type ts from "typescript";
 
 export function nodeBelongsToArgumentList(compiler: typeof ts, node: ts.Node): boolean {
-  if (compiler.isCallExpression(node.parent)) {
-    return node.parent.arguments.some((argument) => argument === node);
-  }
-
-  return false;
+  return compiler.isCallExpression(node.parent) && node.parent.arguments.some((argument) => argument === node);
 }
 
 export function nodeIsChildOfExpressionStatement(compiler: typeof ts, node: ts.Node): boolean {
