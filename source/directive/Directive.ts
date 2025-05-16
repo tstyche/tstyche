74337c69d4aa499c426f9598cda0f8e778fbd829
mrazauskas <tom@mrazauskas.de>
2025-05-16 16:09:19 +0300

    clean up

--- source/directive/Directive.ts
+++ source/directive/Directive.ts
@@ -1,18 +1,14 @@
 import type ts from "typescript";
 
 export class Directive {
-  static isTemplate(compiler: typeof ts, text: string): boolean {
+  static isTemplate(compiler: typeof ts, text: string): boolean | undefined {
     const commentRanges = compiler.getLeadingCommentRanges(text, 0);
 
-    if (commentRanges != null) {
-      return commentRanges.some((range) => {
-        return (
-          range.kind === compiler.SyntaxKind.SingleLineCommentTrivia &&
-          /^\/\/\s*@tstyche-template/.test(text.substring(range.pos, range.end))
-        );
-      });
-    }
-
-    return false;
+    return commentRanges?.some((range) => {
+      return (
+        range.kind === compiler.SyntaxKind.SingleLineCommentTrivia &&
+        /^\/\/\s*@tstyche-template/.test(text.substring(range.pos, range.end))
+      );
+    });
   }
 }
