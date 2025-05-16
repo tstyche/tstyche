17115c9d4010d83251eec69728c8cc8e6f19e11d
mrazauskas <tom@mrazauskas.de>
2025-05-16 12:21:00 +0300

    chore: use `@typedef`

--- scripts/tidy-package-json.cjs
+++ scripts/tidy-package-json.cjs
@@ -1,13 +1,15 @@
+/**
+ * @typedef {Object} Hooks
+ * @property {(_workspace: never, packageConfig: Record<string, unknown>) => void} beforeWorkspacePacking
+ */
+
 const deleteKeys = ["files", "scripts", "devDependencies", "packageManager"];
 
 module.exports = {
   name: "tidy-package-json",
   factory: () => ({
+    /** @type {Hooks} */
     hooks: {
-      /**
-       * @param {never} _workspace
-       * @param {Record<string, unknown>} packageConfig
-       */
       beforeWorkspacePacking(_workspace, packageConfig) {
         for (const [key] of Object.entries(packageConfig)) {
           if (deleteKeys.includes(key)) {
