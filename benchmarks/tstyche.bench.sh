f15b9340cdba4a6e6ed972a1193b1dab54db832b
mrazauskas <tom@mrazauskas.de>
2025-05-26 16:23:49 +0300

    chore: update benchmark script

--- benchmarks/tstyche.bench.sh
+++ benchmarks/tstyche.bench.sh
@@ -6,19 +6,19 @@ hyperfine \
   --warmup 2 \
   'yarn test:examples' \
   --command-name 'warm cache: tstyche examples' \
-  --prepare '' \
+  --prepare 'tstyche --update' \
   'yarn test:examples --target next' \
   --command-name 'warm cache: tstyche examples --target next' \
-  --prepare '' \
+  --prepare 'tstyche --update' \
   'yarn test:examples --target 5.2' \
   --command-name 'warm cache: tstyche examples --target 5.2' \
-  --prepare '' \
+  --prepare 'tstyche --update' \
   'yarn test:examples' \
   --command-name 'cold cache: tstyche examples' \
-  --prepare 'tstyche --prune' \
+  --prepare 'tstyche --prune && tstyche --update' \
   'yarn test:examples --target next' \
   --command-name 'cold cache: tstyche examples --target next' \
-  --prepare 'tstyche --prune' \
+  --prepare 'tstyche --prune && tstyche --update' \
   'yarn test:examples --target 5.2' \
   --command-name 'cold cache: tstyche examples --target 5.2' \
-  --prepare 'tstyche --prune'
+  --prepare 'tstyche --prune && tstyche --update'
