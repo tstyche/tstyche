23ec5a6e3fbe2f6f9feaa78fecf9bbe5afbf8f6b
mrazauskas <tom@mrazauskas.de>
2025-06-02 14:27:34 +0300

    chore: use Volta

--- scripts/tidy-package-json.cjs
+++ scripts/tidy-package-json.cjs
@@ -3,7 +3,7 @@
  * @property {(_workspace: never, packageConfig: Record<string, unknown>) => void} beforeWorkspacePacking
  */
 
-const deleteKeys = ["files", "scripts", "devDependencies", "packageManager"];
+const deleteKeys = ["files", "scripts", "devDependencies", "volta"];
 
 module.exports = {
   name: "tidy-package-json",
