f0b07990b017a101f3b3360df2f531410c35c826
mrazauskas <tom@mrazauskas.de>
2025-06-08 18:16:23 +0300

    chore: use `mise-en-place`

--- mise-tasks/bench.sh
+++ mise-tasks/bench.sh
@@ -6,19 +6,19 @@ hyperfine \
   --warmup 2 \
   'yarn test:examples' \
   --command-name 'warm cache: tstyche examples' \
-  --prepare 'tstyche --update' \
+  --prepare 'yarn tstyche --update' \
   'yarn test:examples --target next' \
   --command-name 'warm cache: tstyche examples --target next' \
-  --prepare 'tstyche --update' \
+  --prepare 'yarn tstyche --update' \
   'yarn test:examples --target 5.2' \
   --command-name 'warm cache: tstyche examples --target 5.2' \
-  --prepare 'tstyche --update' \
+  --prepare 'yarn tstyche --update' \
   'yarn test:examples' \
   --command-name 'cold cache: tstyche examples' \
-  --prepare 'tstyche --prune && tstyche --update' \
+  --prepare 'yarn tstyche --prune && yarn tstyche --update' \
   'yarn test:examples --target next' \
   --command-name 'cold cache: tstyche examples --target next' \
-  --prepare 'tstyche --prune && tstyche --update' \
+  --prepare 'yarn tstyche --prune && yarn tstyche --update' \
   'yarn test:examples --target 5.2' \
   --command-name 'cold cache: tstyche examples --target 5.2' \
-  --prepare 'tstyche --prune && tstyche --update'
+  --prepare 'yarn tstyche --prune && yarn tstyche --update'
