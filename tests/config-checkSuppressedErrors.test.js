85683fdef28062ef33c71e71b7be19e625035dd0
mrazauskas <tom@mrazauskas.de>
2025-06-15 12:14:22 +0300

    option test

---
+++ tests/config-checkSuppressedErrors.test.js
@@ -0,0 +1,63 @@
+import test from "node:test";
+import * as assert from "./__utilities__/assert.js";
+import { clearFixture, getFixtureFileUrl, getTestFileName, writeFixture } from "./__utilities__/fixture.js";
+import { normalizeOutput } from "./__utilities__/output.js";
+import { spawnTyche } from "./__utilities__/tstyche.js";
+
+const testFileText = `// @ts-expect-error Does not work
+console.log(add);`;
+
+const testFileName = getTestFileName(import.meta.url);
+const fixtureUrl = getFixtureFileUrl(testFileName, { generated: true });
+
+await test("'checkSuppressedErrors' config file option", async (t) => {
+  t.afterEach(async () => {
+    await clearFixture(fixtureUrl);
+  });
+
+  await t.test("when enabled", async () => {
+    const config = {
+      checkSuppressedErrors: true,
+    };
+
+    await writeFixture(fixtureUrl, {
+      ["__typetests__/sample.tst.ts"]: testFileText,
+      ["tstyche.config.json"]: JSON.stringify(config, null, 2),
+    });
+
+    const { exitCode, stderr, stdout } = await spawnTyche(fixtureUrl);
+
+    await assert.matchSnapshot(normalizeOutput(stdout), {
+      fileName: `${testFileName}-enabled-stdout`,
+      testFileUrl: import.meta.url,
+    });
+
+    await assert.matchSnapshot(normalizeOutput(stderr), {
+      fileName: `${testFileName}-enabled-stderr`,
+      testFileUrl: import.meta.url,
+    });
+
+    assert.equal(exitCode, 1);
+  });
+
+  await t.test("when disabled", async () => {
+    const config = {
+      checkSuppressedErrors: false,
+    };
+
+    await writeFixture(fixtureUrl, {
+      ["__typetests__/sample.tst.ts"]: testFileText,
+      ["tstyche.config.json"]: JSON.stringify(config, null, 2),
+    });
+
+    const { exitCode, stderr, stdout } = await spawnTyche(fixtureUrl);
+
+    await assert.matchSnapshot(normalizeOutput(stdout), {
+      fileName: `${testFileName}-disabled-stdout`,
+      testFileUrl: import.meta.url,
+    });
+
+    assert.equal(stderr, "");
+    assert.equal(exitCode, 0);
+  });
+});
