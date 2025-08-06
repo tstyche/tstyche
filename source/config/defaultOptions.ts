da9c2bd415408c1a5d63d63da74b04306b570bb4
Tom Mrazauskas <tom@mrazauskas.de>
2025-08-06 16:44:34 +0300

    feat!: enable the `checkSuppressedErrors` option by default (#551)

--- source/config/defaultOptions.ts
+++ source/config/defaultOptions.ts
@@ -4,7 +4,7 @@ import type { ConfigFileOptions } from "./types.js";
 
 export const defaultOptions: Required<ConfigFileOptions> = {
   checkSourceFiles: true,
-  checkSuppressedErrors: false,
+  checkSuppressedErrors: true,
   failFast: false,
   fixtureFileMatch: ["**/__fixtures__/*.{ts,tsx}", "**/fixtures/*.{ts,tsx}"],
   plugins: [],
