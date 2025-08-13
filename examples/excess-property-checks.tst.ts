ca91a24b187546799b9cc8860e92715c55e28988
mrazauskas <tom@mrazauskas.de>
2025-08-13 09:42:24 +0300

    chore: update examples

--- examples/excess-property-checks.tst.ts
+++ examples/excess-property-checks.tst.ts
@@ -1,15 +1,19 @@
 import { expect } from "tstyche";
 
-// // Object literals may only specify known properties
-// expect.fail<{ timeout?: number }>().type.toBeAssignableWith({ silent: true, timeout: 800 });
-// expect.fail({ silent: true, timeout: 800 }).type.toBeAssignableTo<{ timeout?: number }>();
+let config: { timeout?: number } = {};
+
+// @ts-expect-error Object literal may only specify known properties
+config = { silent: true, timeout: 800 };
+
+// Just like above, object literal may only specify known properties
+expect({ silent: true, timeout: 800 }).type.not.toBeAssignableTo<{ timeout?: number }>();
+expect<{ timeout?: number }>().type.not.toBeAssignableWith({ silent: true, timeout: 800 });
 
 // But object types are allowed to have excess properties
-expect<{ timeout?: number }>().type.toBeAssignableWith<{ silent: true; timeout: 800 }>();
 expect<{ silent: true; timeout: 800 }>().type.toBeAssignableTo<{ timeout?: number }>();
+expect<{ timeout?: number }>().type.toBeAssignableWith<{ silent: true; timeout: 800 }>();
 
 // To get around, pass a reference instead of an object literal
 const configSample = { silent: true, timeout: 800 };
 
-expect<{ timeout?: number }>().type.toBeAssignableWith(configSample);
 expect(configSample).type.toBeAssignableTo<{ timeout?: number }>();
