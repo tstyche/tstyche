d4f6c1c6d98ccd84d3b684bbeb0b92718295c551
mrazauskas <tom@mrazauskas.de>
2025-08-21 14:13:05 +0300

    chore: clean up

--- .yarn/plugins/tidy-package-json.cjs
+++ .yarn/plugins/tidy-package-json.cjs
@@ -3,7 +3,7 @@
  * @property {(_workspace: never, packageConfig: Record<string, unknown>) => void} beforeWorkspacePacking
  */
 
-const deleteKeys = ["files", "scripts", "devDependencies", "packageManager"];
+const deleteKeys = ["files", "scripts", "devDependencies"];
 
 module.exports = {
   name: "tidy-package-json",
