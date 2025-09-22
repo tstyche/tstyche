9a20759949a7dbe3b336c05955c74ac178a7065e
mrazauskas <tom@mrazauskas.de>
2025-09-22 11:03:50 +0300

    add example

---
+++ examples/FormFieldGetter.tst.ts
@@ -0,0 +1,15 @@
+import { expect, test } from "tstyche";
+
+type FormFieldGetter = <T, K extends keyof T>(form: T, field: K) => T[K];
+
+const userForm = {
+  name: "Alice",
+  email: "alice@example.com",
+};
+
+test("FormFieldGetter", () => {
+  expect<FormFieldGetter>().type.toBeCallableWith(userForm, "name");
+  expect<FormFieldGetter>().type.toBeCallableWith(userForm, "email");
+
+  expect<FormFieldGetter>().type.not.toBeCallableWith(userForm, "age");
+});
