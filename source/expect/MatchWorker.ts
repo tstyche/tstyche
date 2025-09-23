d17678009da6ff210a855c391997be625b0e40c6
mrazauskas <tom@mrazauskas.de>
2025-09-23 19:20:59 +0300

    refactor!: implement the `.toHaveProperty()` matcher using the ability layer

--- source/expect/MatchWorker.ts
+++ source/expect/MatchWorker.ts
@@ -16,15 +16,6 @@ export class MatchWorker {
     this.assertionNode = assertionNode;
   }
 
-  checkHasApplicableIndexType(sourceNode: ts.Node, targetNode: ts.Node): boolean {
-    const sourceType = this.getType(sourceNode);
-    const targetType = this.getType(targetNode);
-
-    return this.typeChecker
-      .getIndexInfosOfType(sourceType)
-      .some(({ keyType }) => this.typeChecker.isApplicableIndexType(targetType, keyType));
-  }
-
   checkHasProperty(sourceNode: ts.Node, propertyNameText: string): boolean {
     const sourceType = this.getType(sourceNode);
 
