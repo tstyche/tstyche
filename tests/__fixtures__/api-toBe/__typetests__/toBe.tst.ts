4522cd2194492899b0a37061b07932881ef00968
mrazauskas <tom@mrazauskas.de>
2025-10-05 18:00:33 +0300

    chore: update `.toBe()` tests

--- tests/__fixtures__/api-toBe/__typetests__/toBe.tst.ts
+++ tests/__fixtures__/api-toBe/__typetests__/toBe.tst.ts
@@ -14,43 +14,43 @@ declare function getSize(): Size;
 
 test("edge cases", () => {
   expect<any>().type.not.toBe<never>();
-  expect<any>().type.not.toBe<unknown>();
+  expect<any>().type.not.toBe<unknown>(); // fail
 
   expect<{ a: string } | { a: string }>().type.toBe<{ a: string }>();
-  expect<{ a: string } | { a: string }>().type.not.toBe<{ a: string }>();
+  expect<{ a: string } | { a: string }>().type.not.toBe<{ a: string }>(); // fail
 
   expect<{ a: string } | { b: string }>().type.not.toBe<{ a: string }>();
-  expect<{ a: string } | { b: string }>().type.toBe<{ a: string }>();
+  expect<{ a: string } | { b: string }>().type.toBe<{ a: string }>(); // fail
 
   expect<{ a: string } & { a: string }>().type.toBe<{ a: string }>();
-  expect<{ a: string } & { a: string }>().type.not.toBe<{ a: string }>();
+  expect<{ a: string } & { a: string }>().type.not.toBe<{ a: string }>(); // fail
 
   expect<{ a: string } & { b: string }>().type.not.toBe<{ a: string }>();
-  expect<{ a: string } & { b: string }>().type.toBe<{ a: string }>();
+  expect<{ a: string } & { b: string }>().type.toBe<{ a: string }>(); // fail
 
   expect<(({ a: string } & { a: string }) | { a: string }) & { a: string }>().type.toBe<{ a: string }>();
-  expect<(({ a: string } & { a: string }) | { a: string }) & { a: string }>().type.not.toBe<{ a: string }>();
+  expect<(({ a: string } & { a: string }) | { a: string }) & { a: string }>().type.not.toBe<{ a: string }>(); // fail
 
   expect<(({ a: string } & { a: string }) | { a: string }) & { b: string }>().type.not.toBe<{ a: string }>();
-  expect<(({ a: string } & { a: string }) | { a: string }) & { b: string }>().type.toBe<{ a: string }>();
+  expect<(({ a: string } & { a: string }) | { a: string }) & { b: string }>().type.toBe<{ a: string }>(); // fail
 
   expect<{ a: string }>().type.toBe<{ a: string } | { a: string }>();
-  expect<{ a: string }>().type.not.toBe<{ a: string } | { a: string }>();
+  expect<{ a: string }>().type.not.toBe<{ a: string } | { a: string }>(); // fail
 
   expect<{ a: string }>().type.not.toBe<{ a: string } | { b: string }>();
-  expect<{ a: string }>().type.toBe<{ a: string } | { b: string }>();
+  expect<{ a: string }>().type.toBe<{ a: string } | { b: string }>(); // fail
 
   expect<{ a: string }>().type.toBe<{ a: string } & { a: string }>();
-  expect<{ a: string }>().type.not.toBe<{ a: string } & { a: string }>();
+  expect<{ a: string }>().type.not.toBe<{ a: string } & { a: string }>(); // fail
 
   expect<{ a: string }>().type.not.toBe<{ a: string } & { b: string }>();
-  expect<{ a: string }>().type.toBe<{ a: string } & { b: string }>();
+  expect<{ a: string }>().type.toBe<{ a: string } & { b: string }>(); // fail
 
   expect<{ a: string }>().type.toBe<(({ a: string } & { a: string }) | { a: string }) & { a: string }>();
-  expect<{ a: string }>().type.not.toBe<(({ a: string } & { a: string }) | { a: string }) & { a: string }>();
+  expect<{ a: string }>().type.not.toBe<(({ a: string } & { a: string }) | { a: string }) & { a: string }>(); // fail
 
   expect<{ a: string }>().type.not.toBe<(({ a: string } & { a: string }) | { a: string }) & { b: string }>();
-  expect<{ a: string }>().type.toBe<(({ a: string } & { a: string }) | { a: string }) & { b: string }>();
+  expect<{ a: string }>().type.toBe<(({ a: string } & { a: string }) | { a: string }) & { b: string }>(); // fail
 
   expect(Date).type.toBe<typeof Date>();
 });
@@ -65,57 +65,57 @@ test("exact optional property types", () => {
   expect<{ a?: number | undefined }>().type.not.toBeAssignableTo<{ a?: number }>();
 });
 
-describe("source type", () => {
-  test("is the same as target type", () => {
+describe("when source is a type", () => {
+  test("is the same as the target type", () => {
     expect<{ a: string; b: number }>().type.toBe<{ a: string; b: number }>();
     expect<Names>().type.toBe<{ first: string; last?: string }>();
 
-    expect<Names>().type.toBe<{ first: string; last: string }>();
+    expect<Names>().type.toBe<{ first: string; last: string }>(); // fail
   });
 
-  test("is NOT the same as target type", () => {
+  test("is NOT the same as the target type", () => {
     expect<Names>().type.not.toBe<{ first: string; last: string }>();
 
-    expect<Names>().type.not.toBe<{ first: string; last?: string }>();
+    expect<Names>().type.not.toBe<{ first: string; last?: string }>(); // fail
   });
 
-  test("is the same as target expression", () => {
+  test("is the same as type of the target expression", () => {
     expect<Names>().type.toBe(getNames());
     expect<{ first: string; last?: string }>().type.toBe(getNames());
 
-    expect<{ first: string; last: string }>().type.toBe(getNames());
+    expect<{ first: string; last: string }>().type.toBe(getNames()); // fail
   });
 
-  test("is NOT the same as target expression", () => {
+  test("is NOT the same as type of the target expression", () => {
     expect<{ first: string; last: string }>().type.not.toBe(getNames());
 
-    expect<{ first: string; last?: string }>().type.not.toBe(getNames());
+    expect<{ first: string; last?: string }>().type.not.toBe(getNames()); // fail
   });
 });
 
-describe("source expression", () => {
-  test("is the same as target type", () => {
+describe("when source is an expression", () => {
+  test("is the same as the target type", () => {
     expect(getNames()).type.toBe<{ first: string; last?: string }>();
     expect(getNames()).type.toBe<Names>();
 
-    expect(getNames()).type.toBe<{ first: string; last: string }>();
+    expect(getNames()).type.toBe<{ first: string; last: string }>(); // fail
   });
 
-  test("is NOT the same as target type", () => {
+  test("is NOT the same as the target type", () => {
     expect(getNames()).type.not.toBe<{ first: string; last: string }>();
 
-    expect(getNames()).type.not.toBe<{ first: string; last?: string }>();
+    expect(getNames()).type.not.toBe<{ first: string; last?: string }>(); // fail
   });
 
-  test("is the same as target expression", () => {
+  test("is the same as type of the target expression", () => {
     expect({ height: 14, width: 25 }).type.toBe(getSize());
 
-    expect({ height: 14 }).type.toBe(getSize());
+    expect({ height: 14 }).type.toBe(getSize()); // fail
   });
 
-  test("is NOT the same as target expression", () => {
+  test("is NOT the same as type of the target expression", () => {
     expect({ height: 14 }).type.not.toBe(getSize());
 
-    expect({ height: 14, width: 25 }).type.not.toBe(getSize());
+    expect({ height: 14, width: 25 }).type.not.toBe(getSize()); // fail
   });
 });
