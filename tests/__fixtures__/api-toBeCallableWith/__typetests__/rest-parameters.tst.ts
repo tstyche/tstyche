1a39767f43c7757b44b18e7303491e70802629d0
mrazauskas <tom@mrazauskas.de>
2025-10-05 09:13:09 +0300

    chore: add more tests

--- tests/__fixtures__/api-toBeCallableWith/__typetests__/rest-parameters.tst.ts
+++ tests/__fixtures__/api-toBeCallableWith/__typetests__/rest-parameters.tst.ts
@@ -6,7 +6,7 @@ declare function leading(...args: [...Array<string>, boolean]): void;
 declare function middle(...args: [string, ...Array<number>, boolean]): void;
 declare function trailing(x: boolean, ...y: Array<string>): void;
 
-describe("when target is an expression", () => {
+describe("when source is an expression", () => {
   test("is callable with the given argument", () => {
     expect(optional).type.toBeCallableWith("one");
     expect(optional).type.not.toBeCallableWith("one"); // fail
@@ -93,5 +93,101 @@ describe("when target is an expression", () => {
 
     expect(trailing).type.not.toBeCallableWith(false, ...[10, 11]);
     expect(trailing).type.toBeCallableWith(false, ...[10, 11]); // fail
+  });
+});
+
+type Optional = (...args: Array<unknown>) => void;
+
+type Leading = (...args: [...Array<string>, boolean]) => void;
+type Middle = (...args: [string, ...Array<number>, boolean]) => void;
+type Trailing = (x: boolean, ...y: Array<string>) => void;
+
+describe("when source is a type", () => {
+  test("is callable with the given argument", () => {
+    expect<Optional>().type.toBeCallableWith("one");
+    expect<Optional>().type.not.toBeCallableWith("one"); // fail
+
+    expect<Leading>().type.toBeCallableWith(false);
+    expect<Leading>().type.not.toBeCallableWith(false); // fail
+
+    expect<Trailing>().type.toBeCallableWith(true);
+    expect<Trailing>().type.not.toBeCallableWith(true); // fail
+  });
+
+  test("is callable with the given arguments", () => {
+    expect<Optional>().type.toBeCallableWith("one", 2, true);
+    expect<Optional>().type.not.toBeCallableWith("one", 2, true); // fail
+
+    expect<Leading>().type.toBeCallableWith("one", "two", true);
+    expect<Leading>().type.not.toBeCallableWith("one", "two", true); // fail
+
+    expect<Leading>().type.toBeCallableWith(...["one", "two"], true);
+    expect<Leading>().type.not.toBeCallableWith(...["one", "two"], true); // fail
+
+    expect<Middle>().type.toBeCallableWith("one", 123, 456, true);
+    expect<Middle>().type.not.toBeCallableWith("one", 123, 456, true); // fail
+
+    expect<Middle>().type.toBeCallableWith(...["one", 123, 456, true]);
+    expect<Middle>().type.not.toBeCallableWith(...["one", 123, 456, true]); // fail
+
+    expect<Trailing>().type.toBeCallableWith(false, "ten", "eleven");
+    expect<Trailing>().type.not.toBeCallableWith(false, "ten", "eleven"); // fail
+
+    expect<Trailing>().type.toBeCallableWith(false, ...["ten", "eleven"]);
+    expect<Trailing>().type.not.toBeCallableWith(false, ...["ten", "eleven"]); // fail
+  });
+
+  test("is callable without arguments", () => {
+    expect<Optional>().type.toBeCallableWith();
+    expect<Optional>().type.not.toBeCallableWith(); // fail
+  });
+
+  test("is not callable without arguments", () => {
+    expect<Leading>().type.not.toBeCallableWith();
+    expect<Leading>().type.toBeCallableWith(); // fail: Source has 0 element(s) but target requires 1.
+
+    expect<Middle>().type.not.toBeCallableWith();
+    expect<Middle>().type.toBeCallableWith(); // fail: Expected at least 1 arguments, but got 0.
+
+    expect<Trailing>().type.not.toBeCallableWith();
+    expect<Trailing>().type.toBeCallableWith(); // fail: Expected at least 1 arguments, but got 0.
+  });
+
+  test("is not callable with the given arguments", () => {
+    expect<Leading>().type.not.toBeCallableWith("one", "two");
+    expect<Leading>().type.toBeCallableWith("one", "two"); // fail
+
+    expect<Leading>().type.not.toBeCallableWith(...["one", "two"]);
+    expect<Leading>().type.toBeCallableWith(...["one", "two"]); // fail
+
+    expect<Leading>().type.not.toBeCallableWith(3, 4, true);
+    expect<Leading>().type.toBeCallableWith(3, 4, true); // fail
+
+    expect<Leading>().type.not.toBeCallableWith(...[3, 4], true);
+    expect<Leading>().type.toBeCallableWith(...[3, 4], true); // fail
+
+    expect<Middle>().type.not.toBeCallableWith("one", 2, 3);
+    expect<Middle>().type.toBeCallableWith("one", 2, 3); // fail
+
+    expect<Middle>().type.not.toBeCallableWith(...["one", 2, 3]);
+    expect<Middle>().type.toBeCallableWith(...["one", 2, 3]); // fail
+
+    expect<Middle>().type.not.toBeCallableWith("one", "two", "three", true);
+    expect<Middle>().type.toBeCallableWith("one", "two", "three", true); // fail
+
+    expect<Middle>().type.not.toBeCallableWith(...["one", "two", "three", true]);
+    expect<Middle>().type.toBeCallableWith(...["one", "two", "three", true]); // fail
+
+    expect<Trailing>().type.not.toBeCallableWith("ten", "eleven");
+    expect<Trailing>().type.toBeCallableWith("ten", "eleven"); // fail
+
+    expect<Trailing>().type.not.toBeCallableWith(...["ten", "eleven"]);
+    expect<Trailing>().type.toBeCallableWith(...["ten", "eleven"]); // fail
+
+    expect<Trailing>().type.not.toBeCallableWith(false, 10, 11);
+    expect<Trailing>().type.toBeCallableWith(false, 10, 11); // fail
+
+    expect<Trailing>().type.not.toBeCallableWith(false, ...[10, 11]);
+    expect<Trailing>().type.toBeCallableWith(false, ...[10, 11]); // fail
   });
 });
