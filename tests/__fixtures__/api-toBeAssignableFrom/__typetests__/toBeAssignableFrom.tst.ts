5480945033da9a4a8cb382e178e75581ef59056e
mrazauskas <tom@mrazauskas.de>
2025-10-05 17:54:13 +0300

    chore: update assignability tests

--- tests/__fixtures__/api-toBeAssignableFrom/__typetests__/toBeAssignableFrom.tst.ts
+++ tests/__fixtures__/api-toBeAssignableFrom/__typetests__/toBeAssignableFrom.tst.ts
@@ -5,59 +5,59 @@ interface Names {
   last?: string;
 }
 
-describe("source type", () => {
-  test("is assignable target expression?", () => {
+describe("when source is a type", () => {
+  test("is assignable from type of the target expression", () => {
     expect<Names>().type.toBeAssignableFrom({ first: "Rose" });
     expect<Names>().type.toBeAssignableFrom({ first: "Rose", last: "Smith" });
     expect<Names>().type.toBeAssignableFrom({ first: "Rose", last: undefined });
 
-    expect<Names>().type.toBeAssignableFrom({ middle: "O." });
+    expect<Names>().type.toBeAssignableFrom({ middle: "O." }); // fail
   });
 
-  test("is NOT assignable target expression?", () => {
+  test("is NOT assignable from type of the target expression", () => {
     expect<Names>().type.not.toBeAssignableFrom({ middle: "O." });
 
-    expect<Names>().type.not.toBeAssignableFrom({ first: "Rose" });
+    expect<Names>().type.not.toBeAssignableFrom({ first: "Rose" }); // fail
   });
 
-  test("is assignable target type?", () => {
+  test("is assignable from the target type", () => {
     expect<Names>().type.toBeAssignableFrom<{ first: string }>();
     expect<Names>().type.toBeAssignableFrom<{ first: string; last: string }>();
     expect<Names>().type.toBeAssignableFrom<{ first: string; last: undefined }>();
     expect<Names>().type.toBeAssignableFrom<{ first: string; last?: string }>();
 
-    expect<Names>().type.toBeAssignableFrom<{ middle: string }>();
+    expect<Names>().type.toBeAssignableFrom<{ middle: string }>(); // fail
   });
 
-  test("is NOT assignable target type?", () => {
+  test("is NOT assignable from the target type", () => {
     expect<Names>().type.not.toBeAssignableFrom<{ middle: string }>();
 
-    expect<Names>().type.not.toBeAssignableFrom<{ first: string }>();
+    expect<Names>().type.not.toBeAssignableFrom<{ first: string }>(); // fail
   });
 });
 
-describe("source expression", () => {
-  test("is assignable target expression?", () => {
+describe("when source is an expression", () => {
+  test("is assignable from type of the target expression.", () => {
     expect({ first: "Jane", last: "Swan" }).type.toBeAssignableFrom({ first: "Rose", last: "Smith" });
 
-    expect({ first: "Jane", last: "Swan" }).type.toBeAssignableFrom({ middle: "O." });
+    expect({ first: "Jane", last: "Swan" }).type.toBeAssignableFrom({ middle: "O." }); // fail
   });
 
-  test("is NOT assignable target expression?", () => {
+  test("is NOT assignable from type of the target expression.", () => {
     expect({ first: "Jane", last: "Swan" }).type.not.toBeAssignableFrom({ middle: "O." });
 
-    expect({ first: "Jane" }).type.not.toBeAssignableFrom({ first: "Rose" });
+    expect({ first: "Jane" }).type.not.toBeAssignableFrom({ first: "Rose" }); // fail
   });
 
-  test("is assignable target type?", () => {
+  test("is assignable from the target type", () => {
     expect({ first: "Jane", last: "Swan" }).type.toBeAssignableFrom<{ first: string; last: string }>();
 
-    expect({ first: "Jane", last: "Swan" }).type.toBeAssignableFrom<{ middle: string }>();
+    expect({ first: "Jane", last: "Swan" }).type.toBeAssignableFrom<{ middle: string }>(); // fail
   });
 
-  test("is NOT assignable type?", () => {
+  test("is NOT assignable from the target type", () => {
     expect({ first: "Jane", last: "Swan" }).type.not.toBeAssignableFrom<{ middle: string }>();
 
-    expect({ first: "Jane" }).type.not.toBeAssignableFrom<{ first: string }>();
+    expect({ first: "Jane" }).type.not.toBeAssignableFrom<{ first: string }>(); // fail
   });
 });
--- tests/__fixtures__/api-toBeAssignableTo/__typetests__/toBeAssignableTo.tst.ts
+++ tests/__fixtures__/api-toBeAssignableTo/__typetests__/toBeAssignableTo.tst.ts
@@ -5,60 +5,60 @@ interface Names {
   last?: string;
 }
 
-describe("source type", () => {
-  test("is assignable to target expression?", () => {
+describe("when source is a type", () => {
+  test("is assignable to type of the target expression", () => {
     expect<{ first: string }>().type.toBeAssignableTo({ first: "Jane" });
 
-    expect<{ first?: string }>().type.toBeAssignableTo({ first: "Jane" });
+    expect<{ first?: string }>().type.toBeAssignableTo({ first: "Jane" }); // fail
   });
 
-  test("is NOT assignable to target expression?", () => {
+  test("is NOT assignable to type of the target expression", () => {
     expect<Names>().type.not.toBeAssignableTo({ first: "Rose", last: "Smith" });
     expect<Names>().type.not.toBeAssignableTo({ first: "Rose", last: undefined });
     expect<Names>().type.not.toBeAssignableTo({ middle: "O." });
 
-    expect<{ first: string }>().type.not.toBeAssignableTo({ first: "Jane" });
+    expect<{ first: string }>().type.not.toBeAssignableTo({ first: "Jane" }); // fail
   });
 
-  test("is assignable to target type?", () => {
+  test("is assignable to the target type", () => {
     expect<Names>().type.toBeAssignableTo<{ first: string }>();
     expect<Names>().type.toBeAssignableTo<{ first: string; last?: string }>();
 
-    expect<Names>().type.toBeAssignableTo<{ middle: string }>();
+    expect<Names>().type.toBeAssignableTo<{ middle: string }>(); // fail
   });
 
-  test("is NOT assignable to target type?", () => {
+  test("is NOT assignable to the target type", () => {
     expect<Names>().type.not.toBeAssignableTo<{ first: string; last: string }>();
     expect<Names>().type.not.toBeAssignableTo<{ first: string; last: undefined }>();
     expect<Names>().type.not.toBeAssignableTo<{ middle: string }>();
 
-    expect<Names>().type.not.toBeAssignableTo<{ first: string }>();
+    expect<Names>().type.not.toBeAssignableTo<{ first: string }>(); // fail
   });
 });
 
-describe("source expression", () => {
-  test("is assignable to target expression?", () => {
+describe("when source is an expression", () => {
+  test("is assignable to type of the target expression", () => {
     expect({ first: "Jane", last: "Swan" }).type.toBeAssignableTo({ first: "Rose", last: "Smith" });
 
-    expect({ first: "Jane", last: "Swan" }).type.toBeAssignableTo({ first: "Rose" });
+    expect({ first: "Jane", last: "Swan" }).type.toBeAssignableTo({ first: "Rose" }); // fail
   });
 
-  test("is NOT assignable to target expression?", () => {
+  test("is NOT assignable to type of the target expression", () => {
     expect({ first: "Jane", last: "Swan" }).type.not.toBeAssignableTo({ first: "Rose" });
 
-    expect({ first: "Jane" }).type.not.toBeAssignableTo({ first: "Rose" });
+    expect({ first: "Jane" }).type.not.toBeAssignableTo({ first: "Rose" }); // fail
   });
 
-  test("is assignable to target type?", () => {
+  test("is assignable to the target type", () => {
     expect({ first: "Jane", last: "Swan" }).type.toBeAssignableTo<{ first: string; last: string }>();
     expect({ first: "Jane", last: "Swan" }).type.toBeAssignableTo<{ first: string; last?: string }>();
 
-    expect({ first: "Jane", last: "Swan" }).type.toBeAssignableTo<{ middle: string }>();
+    expect({ first: "Jane", last: "Swan" }).type.toBeAssignableTo<{ middle: string }>(); // fail
   });
 
-  test("is NOT assignable to type?", () => {
+  test("is NOT assignable to the target type", () => {
     expect({ first: "Jane", last: "Swan" }).type.not.toBeAssignableTo<{ middle: string }>();
 
-    expect({ first: "Jane" }).type.not.toBeAssignableTo<{ first: string }>();
+    expect({ first: "Jane" }).type.not.toBeAssignableTo<{ first: string }>(); // fail
   });
 });
