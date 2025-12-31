bc466a63113259f8a5ec3a13a1900f128c57c3b1
Tom Mrazauskas <tom@mrazauskas.de>
2025-12-31 14:02:02 +0200

    fix(structure): compare template literal types (#648)

--- tests/__fixtures__/api-toBe/__typetests__/template-literals.tst.ts
+++ tests/__fixtures__/api-toBe/__typetests__/template-literals.tst.ts
@@ -1,16 +1,23 @@
 import { expect, test } from "tstyche";
 
-test("element arity", () => {
-  type A = `a-${string}`;
-  type B = `b-${number}`;
+test("signatures", () => {
+  type A = <T extends string>() => `a-${T}`;
+  type B = <T extends number>() => `b-${T}`;
+  type C = <T extends string>() => `a-${T}-${T}`;
 
-  expect<A>().type.toBe<`a-${string}`>();
-  expect<A>().type.not.toBe<`a-`>();
-  expect<A>().type.not.toBe<`a-${string}-${string}`>();
+  expect<A>().type.toBe<<T extends string>() => `a-${T}`>();
+  expect<A>().type.not.toBe<<T extends number>() => `a-${T}`>();
+  expect<A>().type.not.toBe<<T extends number>() => number>();
 
-  expect<B>().type.toBe<`b-${number}`>();
-  expect<B>().type.not.toBe<`b-`>();
-  expect<B>().type.not.toBe<`b-${number}-${number}`>();
+  expect<B>().type.toBe<<T extends number>() => `b-${T}`>();
+  expect<B>().type.not.toBe<<T extends string>() => `b-${T}`>();
+  expect<B>().type.not.toBe<<T extends string>() => string>();
+
+  expect<A>().type.not.toBe<B>();
+
+  expect<C>().type.toBe<<T extends string>() => `a-${T}-${T}`>();
+  expect<C>().type.not.toBe<<T extends number>() => `a-${T}-${T}`>();
+  expect<C>().type.not.toBe<<T extends string>() => `a-${T}`>();
 });
 
 test("element types", () => {
