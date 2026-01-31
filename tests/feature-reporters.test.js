086db34bd9ef8d1a482bcf088601c33c0c18dad2
mrazauskas <tom@mrazauskas.de>
2026-01-31 16:30:46 +0200

    add more tests

--- tests/feature-reporters.test.js
+++ tests/feature-reporters.test.js
@@ -22,6 +22,31 @@ test("is number?", () => {
 });
 `;
 
+const fixmeTestText = `import { expect, test } from "tstyche";
+// @tstyche fixme
+test("is string?", () => {
+  expect<string>().type.toBe<string>();
+});
+// @tstyche fixme
+test("is number?", () => {
+  expect<string>().type.toBe<number>();
+});
+`;
+const skipTestText = `import { expect, test } from "tstyche";
+test("is string?", () => {
+  expect<string>().type.toBe<string>();
+});
+test.skip("is number?", () => {
+  expect<string>().type.toBe<number>();
+});
+`;
+const todoTestText = `import { expect, test } from "tstyche";
+test("is string?", () => {
+  expect<string>().type.toBe<string>();
+});
+test.todo("is number?");
+`;
+
 const testFileName = getTestFileName(import.meta.url);
 const fixtureUrl = getFixtureFileUrl(testFileName, { generated: true });
 
@@ -108,6 +133,60 @@ await test("reporters", async (t) => {
         });
 
         assert.equal(exitCode, 1);
+      });
+
+      await t.test("'// @tstyche fixme' test case", async () => {
+        await writeFixture(fixtureUrl, {
+          ["__typetests__/feature.tst.ts"]: fixmeTestText,
+        });
+
+        const { exitCode, stderr, stdout } = await spawnTyche(fixtureUrl, ["--reporters", reporter]);
+
+        await assert.matchSnapshot(normalizeOutput(stderr), {
+          fileName: `${testFileName}-fixme-stderr`,
+          testFileUrl: import.meta.url,
+        });
+
+        await assert.matchSnapshot(normalizeOutput(stdout), {
+          fileName: `${testFileName}-${reporter}-fixme-stdout`,
+          testFileUrl: import.meta.url,
+        });
+
+        assert.equal(exitCode, 1);
+      });
+
+      await t.test("'.skip' test case", async () => {
+        await writeFixture(fixtureUrl, {
+          ["__typetests__/feature.tst.ts"]: skipTestText,
+        });
+
+        const { exitCode, stderr, stdout } = await spawnTyche(fixtureUrl, ["--reporters", reporter]);
+
+        assert.equal(stderr, "");
+
+        await assert.matchSnapshot(normalizeOutput(stdout), {
+          fileName: `${testFileName}-${reporter}-skip-stdout`,
+          testFileUrl: import.meta.url,
+        });
+
+        assert.equal(exitCode, 0);
+      });
+
+      await t.test("'.todo' test case", async () => {
+        await writeFixture(fixtureUrl, {
+          ["__typetests__/feature.tst.ts"]: todoTestText,
+        });
+
+        const { exitCode, stderr, stdout } = await spawnTyche(fixtureUrl, ["--reporters", reporter]);
+
+        assert.equal(stderr, "");
+
+        await assert.matchSnapshot(normalizeOutput(stdout), {
+          fileName: `${testFileName}-${reporter}-todo-stdout`,
+          testFileUrl: import.meta.url,
+        });
+
+        assert.equal(exitCode, 0);
       });
     });
   }
