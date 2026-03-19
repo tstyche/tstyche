01ee3a68f22b62c4d6136ce67c4d01e804f8124c
mrazauskas <tom@mrazauskas.de>
2026-03-19 13:45:38 +0200

    refactor: clean up code

--- source/json/JsonNode.ts
+++ source/json/JsonNode.ts
@@ -10,33 +10,31 @@ export class JsonNode {
     this.text = text;
   }
 
-  getValue(options: { expectsIdentifier: true }): string | undefined;
   getValue(): OptionValue;
+  getValue(options: { expectsIdentifier: true }): string | undefined;
   getValue(options?: { expectsIdentifier?: boolean }): OptionValue {
-    if (this.text == null) {
-      return undefined;
+    if (this.text != null) {
+      if (/^['"]/.test(this.text)) {
+        return this.text.slice(1, -1).replaceAll("\\", "");
+      }
+
+      if (options?.expectsIdentifier) {
+        return this.text;
+      }
+
+      if (this.text === "true") {
+        return true;
+      }
+
+      if (this.text === "false") {
+        return false;
+      }
+
+      if (/^\d/.test(this.text)) {
+        return Number.parseFloat(this.text);
+      }
     }
 
-    if (/^['"]/.test(this.text)) {
-      return this.text.slice(1, -1).replaceAll("\\", "");
-    }
-
-    if (options?.expectsIdentifier) {
-      return this.text;
-    }
-
-    if (this.text === "true") {
-      return true;
-    }
-
-    if (this.text === "false") {
-      return false;
-    }
-
-    if (/^\d/.test(this.text)) {
-      return Number.parseFloat(this.text);
-    }
-
-    return undefined;
+    return;
   }
 }
