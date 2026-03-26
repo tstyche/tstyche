3f34296f283a3f4521463c17e905f183414b8b21
Federico Biccheddu <433819+f15u@users.noreply.github.com>
2026-03-26 13:13:40 +0000

    dx: add `flake.nix` and `direnv` support (#712)
    Co-authored-by: Tom Mrazauskas <tom@mrazauskas.de>

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+  outputs = {nixpkgs, ...}: let
+    forAllSystems = function:
+      nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (
+        system: function nixpkgs.legacyPackages.${system}
+      );
+  in {
+    formatter = forAllSystems (pkgs: pkgs.alejandra);
+    devShells = forAllSystems (pkgs: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          corepack_24
+          nodejs_24
+
+          mise
+          python3
+        ];
+      };
+    });
+  };
+}
