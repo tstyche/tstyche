dfd2874c50ddf037955a31e92362a3db041c1b7f
mrazauskas <tom@mrazauskas.de>
2026-04-07 13:29:49 +0300

    chore: update example
    Signed-off-by: mrazauskas <tom@mrazauskas.de>

--- examples/callbacks.tst.ts
+++ examples/callbacks.tst.ts
@@ -1,11 +1,10 @@
 import { expect, test } from "tstyche";
 
-declare function fetchData(url: string, callback: (error: Error | null) => void): void;
+declare function fetchData(url: string, callback: (error: Error | null, data: unknown) => void): void;
 
 test("fetchData", () => {
-  fetchData("https://api.example.com", (error) => {
+  fetchData("https://api.example.com", (error, data) => {
     expect(error).type.toBe<Error | null>();
+    expect(data).type.toBe<unknown>();
   });
-
-  fetchData("https://api.example.com", (error) => expect(error).type.toBe<Error | null>());
 });
