f7b06eee1ee00e3af4cad37b41c5df0835caeafe
mrazauskas <tom@mrazauskas.de>
2026-04-08 11:17:41 +0300

    refactor: use `fs.mkdtemp()`
    Signed-off-by: mrazauskas <tom@mrazauskas.de>

--- source/store/PackageService.ts
+++ source/store/PackageService.ts
@@ -26,7 +26,7 @@ export class PackageService {
     const response = await this.#fetcher.get(request, diagnostic);
 
     if (response?.body != null) {
-      const targetPath = `${packagePath}-${Date.now().toString(36)}`;
+      const targetPath = await fs.mkdtemp(`${packagePath}-`);
 
       const stream = response.body.pipeThrough<Uint8Array>(new DecompressionStream("gzip"));
       const tarReader = new TarReader(stream);
