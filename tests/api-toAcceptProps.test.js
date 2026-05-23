03977a7d02913f51e7476c455d6975f0ae6182b4
mrazauskas <tom@mrazauskas.de>
2026-05-23 18:20:57 +0300

    more tests
    Signed-off-by: mrazauskas <tom@mrazauskas.de>

--- tests/api-toAcceptProps.test.js
+++ tests/api-toAcceptProps.test.js
@@ -1,7 +1,7 @@
 import test from "node:test";
 import * as tstyche from "tstyche";
 import * as assert from "./__utilities__/assert.js";
-import { getFixtureFileUrl, getTestFileName } from "./__utilities__/fixture.js";
+import { clearFixture, getFixtureFileUrl, getTestFileName, writeFixture } from "./__utilities__/fixture.js";
 import { normalizeOutput } from "./__utilities__/output.js";
 import { spawnTyche } from "./__utilities__/tstyche.js";
 
@@ -72,6 +72,116 @@ await test("toAcceptProps", async (t) => {
 
     await assert.matchSnapshot(normalizeOutput(stdout), {
       fileName: `${testFileName}-special-cases-stdout`,
+      testFileUrl: import.meta.url,
+    });
+
+    assert.equal(exitCode, 1);
+  });
+
+  await t.test("handles missing semicolons", async (t) => {
+    const toAcceptPropsText = `import { expect, test } from "tstyche"
+
+interface ButtonProps {
+  text: string
+  type?: "reset" | "submit"
+}
+
+function Button({ text, type }: ButtonProps) {
+  return <button type={type}>{text}</button>
+}
+
+test("accepts props?", () => {
+  expect(Button).type.toAcceptProps({ text: "Send" })
+  expect(Button).type.toAcceptProps({ text: "Clear", type: "reset" as const })
+
+  expect(Button).type.not.toAcceptProps({ text: "Download", type: "button" as const })
+  expect(Button).type.not.toAcceptProps({})
+})
+`;
+
+    const tsconfig = {
+      extends: "../../../tsconfig.json",
+      compilerOptions: {
+        jsx: "react-jsx",
+      },
+      include: ["**/*"],
+    };
+
+    const fixtureUrl = getFixtureFileUrl(testFileName, { generated: true });
+
+    t.after(async () => {
+      await clearFixture(fixtureUrl);
+    });
+
+    await writeFixture(fixtureUrl, {
+      ["__typetests__/toAcceptProps.tst.tsx"]: toAcceptPropsText,
+      ["__typetests__/tsconfig.json"]: JSON.stringify(tsconfig),
+    });
+
+    const { exitCode, stderr, stdout } = await spawnTyche(fixtureUrl);
+
+    assert.equal(stderr, "");
+
+    await assert.matchSnapshot(normalizeOutput(stdout), {
+      fileName: `${testFileName}-missing-semicolons-stdout`,
+      testFileUrl: import.meta.url,
+    });
+
+    assert.equal(exitCode, 0);
+  });
+
+  await t.test("handles trailing comma", async (t) => {
+    const toAcceptPropsText = `import { expect, test } from "tstyche";
+
+interface ButtonProps {
+  text: string;
+  type?: "reset" | "submit";
+}
+
+function Button({ text, type }: ButtonProps) {
+  return <button type={type}>{text}</button>;
+}
+
+test("accepts props?", () => {
+  expect(
+    Button,
+  ).type.not.toAcceptProps({ text: "Send" }); // fail
+  expect(Button,  \n  ).type.not.toAcceptProps({ text: "Clear", type: "reset" as const }); // fail
+
+  expect(
+    Button  ,
+  ).type.toAcceptProps({}); // fail
+});
+`;
+
+    const tsconfig = {
+      extends: "../../../tsconfig.json",
+      compilerOptions: {
+        jsx: "react-jsx",
+      },
+      include: ["**/*"],
+    };
+
+    const fixtureUrl = getFixtureFileUrl(testFileName, { generated: true });
+
+    t.after(async () => {
+      await clearFixture(fixtureUrl);
+    });
+
+    await writeFixture(fixtureUrl, {
+      ["__typetests__/toAcceptPropsText.tst.tsx"]: toAcceptPropsText,
+      ["__typetests__/tsconfig.json"]: JSON.stringify(tsconfig),
+    });
+
+    const { exitCode, stderr, stdout } = await spawnTyche(fixtureUrl);
+
+    await assert.matchSnapshot(stderr, {
+      fileName: `${testFileName}-trailing-comma-stderr`,
+      testFileUrl: import.meta.url,
+    });
+
+    await assert.matchSnapshot(normalizeOutput(stdout), {
+      fileName: `${testFileName}-trailing-comma-stdout`,
       testFileUrl: import.meta.url,
     });
 
