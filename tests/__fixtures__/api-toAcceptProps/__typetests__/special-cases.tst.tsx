a6fa5431bb8728d9009636e218ff748249743e76
mrazauskas <tom@mrazauskas.de>
2026-05-25 11:27:02 +0300

    more tests
    Signed-off-by: mrazauskas <tom@mrazauskas.de>

--- tests/__fixtures__/api-toAcceptProps/__typetests__/special-cases.tst.tsx
+++ tests/__fixtures__/api-toAcceptProps/__typetests__/special-cases.tst.tsx
@@ -1,24 +1,7 @@
+import type { ReactNode } from "react";
 import { describe, expect, test } from "tstyche";
 
 describe("special cases", () => {
-  test("when the 'props' argument has a default value", () => {
-    function Default(props = { one: "sample" }) {
-      return <>{`${props}`}</>;
-    }
-
-    expect(Default).type.toAcceptProps({ one: "sample" });
-    expect(Default).type.not.toAcceptProps({ one: "sample" }); // fail
-
-    expect(Default).type.not.toAcceptProps({});
-    expect(Default).type.toAcceptProps({}); // fail
-
-    expect(Default).type.not.toAcceptProps({ two: false });
-    expect(Default).type.toAcceptProps({ two: false }); // fail
-
-    expect(Default).type.not.toAcceptProps({ one: false });
-    expect(Default).type.toAcceptProps({ one: false }); // fail
-  });
-
   test("when the 'props' is a union", () => {
     type One = { one: string; two?: never };
     type Other = { two: string; one?: never };
@@ -46,4 +29,34 @@ describe("special cases", () => {
     expect(OneOrTheOther).type.not.toAcceptProps({ three: false });
     expect(OneOrTheOther).type.toAcceptProps({ three: false }); // fail
   });
+
+  test("when component requires children", () => {
+    interface CardProps {
+      title: string;
+      children: ReactNode;
+    }
+
+    function Card({ title, children }: CardProps) {
+      return (
+        <>
+          <h2>{title}</h2>
+          {children}
+        </>
+      );
+    }
+
+    expect(Card).type.toAcceptProps({ title: "Hello", children: "Click me" });
+    expect(Card).type.not.toAcceptProps({ title: "Hello", children: "Click me" }); //fail
+
+    expect(Card).type.toAcceptProps({ title: "Hello", children: <span>Click me</span> });
+    expect(Card).type.not.toAcceptProps({ title: "Hello", children: <span>Click me</span> }); // fail
+
+    expect(Card).type.not.toAcceptProps({ title: "Hello" });
+    expect(Card).type.toAcceptProps({ title: "Hello" }); // fail
+
+    expect(Card).type.not.toAcceptProps({});
+    expect(Card).type.toAcceptProps({}); // fail
+  });
 });
+
+// kebab-case
