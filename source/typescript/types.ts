69615e88edab2afa768e29332912c2015df19783
mrazauskas <tom@mrazauskas.de>
2026-06-08 11:50:51 +0300

    fix bundling error
    Signed-off-by: mrazauskas <tom@mrazauskas.de>

--- source/typescript/types.ts
+++ source/typescript/types.ts
@@ -1,41 +1,41 @@
-import type * as ast from "@typescript/native-preview/unstable/ast";
+import type * as tsAst from "@typescript/native-preview/unstable/ast";
 import type ts6 from "typescript";
 import type { CompatTypeScript } from "./CompatTypeScript.js";
 import type { NativeTypeScript } from "./NativeTypeScript.js";
 
-export type CallExpression = ast.CallExpression | ts6.CallExpression;
+export type CallExpression = tsAst.CallExpression | ts6.CallExpression;
 export type CallbackFunction =
-  | (ast.ArrowFunction | ast.FunctionExpression)
+  | (tsAst.ArrowFunction | tsAst.FunctionExpression)
   | (ts6.ArrowFunction | ts6.FunctionExpression);
-export type CommentRange = ast.CommentRange | ts6.CommentRange;
-export type Decorator = ast.Decorator | ts6.Decorator;
-export type Expression = ast.Expression | ts6.Expression;
-export type ExpressionStatement = ast.ExpressionStatement | ts6.ExpressionStatement;
-export type ExpressionWithTypeArguments = ast.ExpressionWithTypeArguments | ts6.ExpressionWithTypeArguments;
-export type Identifier = ast.Identifier | ts6.Identifier;
-export type ImportDeclaration = ast.ImportDeclaration | ts6.ImportDeclaration;
-export type NamespaceImport = ast.NamespaceImport | ts6.NamespaceImport;
-export type NamedImports = ast.NamedImports | ts6.NamedImports;
-export type Node = ast.Node | ts6.Node;
+export type CommentRange = tsAst.CommentRange | ts6.CommentRange;
+export type Decorator = tsAst.Decorator | ts6.Decorator;
+export type Expression = tsAst.Expression | ts6.Expression;
+export type ExpressionStatement = tsAst.ExpressionStatement | ts6.ExpressionStatement;
+export type ExpressionWithTypeArguments = tsAst.ExpressionWithTypeArguments | ts6.ExpressionWithTypeArguments;
+export type Identifier = tsAst.Identifier | ts6.Identifier;
+export type ImportDeclaration = tsAst.ImportDeclaration | ts6.ImportDeclaration;
+export type NamespaceImport = tsAst.NamespaceImport | ts6.NamespaceImport;
+export type NamedImports = tsAst.NamedImports | ts6.NamedImports;
+export type Node = tsAst.Node | ts6.Node;
 
-export type NodeArray<T extends Node> = T extends ast.Node
-  ? ast.NodeArray<T>
+export type NodeArray<T extends Node> = T extends tsAst.Node
+  ? tsAst.NodeArray<T>
   : T extends ts6.Node
     ? ts6.NodeArray<T>
     : never;
 
-export type NumericLiteral = ast.NumericLiteral | ts6.NumericLiteral;
-export type ObjectLiteralExpression = ast.ObjectLiteralExpression | ts6.ObjectLiteralExpression;
-export type ParenthesizedExpression = ast.ParenthesizedExpression | ts6.ParenthesizedExpression;
-export type PropertyAccessExpression = ast.PropertyAccessExpression | ts6.PropertyAccessExpression;
-export type PropertyAssignment = ast.PropertyAssignment | ts6.PropertyAssignment;
-export type RegularExpressionLiteral = ast.RegularExpressionLiteral | ts6.RegularExpressionLiteral;
-export type SourceFile = ast.SourceFile | ts6.SourceFile;
-export type SpreadAssignment = ast.SpreadAssignment | ts6.SpreadAssignment;
-export type StringLiteral = ast.StringLiteral | ts6.StringLiteral;
-export type StringLiteralLikeNode = ast.StringLiteralLikeNode | ts6.StringLiteralLike;
-export type TupleTypeNode = ast.TupleTypeNode | ts6.TupleTypeNode;
-export type TypeNode = ast.TypeNode | ts6.TypeNode;
-export type TypeReferenceNode = ast.TypeReferenceNode | ts6.TypeReferenceNode;
+export type NumericLiteral = tsAst.NumericLiteral | ts6.NumericLiteral;
+export type ObjectLiteralExpression = tsAst.ObjectLiteralExpression | ts6.ObjectLiteralExpression;
+export type ParenthesizedExpression = tsAst.ParenthesizedExpression | ts6.ParenthesizedExpression;
+export type PropertyAccessExpression = tsAst.PropertyAccessExpression | ts6.PropertyAccessExpression;
+export type PropertyAssignment = tsAst.PropertyAssignment | ts6.PropertyAssignment;
+export type RegularExpressionLiteral = tsAst.RegularExpressionLiteral | ts6.RegularExpressionLiteral;
+export type SourceFile = tsAst.SourceFile | ts6.SourceFile;
+export type SpreadAssignment = tsAst.SpreadAssignment | ts6.SpreadAssignment;
+export type StringLiteral = tsAst.StringLiteral | ts6.StringLiteral;
+export type StringLiteralLikeNode = tsAst.StringLiteralLikeNode | ts6.StringLiteralLike;
+export type TupleTypeNode = tsAst.TupleTypeNode | ts6.TupleTypeNode;
+export type TypeNode = tsAst.TypeNode | ts6.TypeNode;
+export type TypeReferenceNode = tsAst.TypeReferenceNode | ts6.TypeReferenceNode;
 
 export type TypeScript = NativeTypeScript | CompatTypeScript;
