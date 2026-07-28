1786c8fc4eff869179811a10bb042ec09a364b39
mrazauskas <tom@mrazauskas.de>
2026-07-28 16:05:10 +0300

    Revert "add "context canceled" retries"
    This reverts commit 505798ab75ecac5a09f1c617222afc1de7ea82a5.
    
    Signed-off-by: mrazauskas <tom@mrazauskas.de>

--- tests/__utilities__/tstyche.js
+++ tests/__utilities__/tstyche.js
@@ -8,58 +8,45 @@ import process from "node:process";
  * @returns {Promise<{ exitCode: number | null, stderr: string, stdout: string }>}
  */
 export function spawnTyche(fixtureUrl, args = [], options = {}) {
-  const maxRetries = 3;
-  const baseDelay = 500;
-
-  let attempt = 0;
-
   return new Promise((resolve, reject) => {
-    const runWithRetry = () => {
-      attempt += 1;
-
-      /** @type {NodeJS.ProcessEnv} */
-      const env = {
-        ["TSTYCHE_NO_COLOR"]: "true",
-        ["TSTYCHE_STORE_PATH"]: "./.store",
-        ...process.env,
-        ...options.env,
-      };
-
-      const tstyche = spawn(["tstyche", ...args].join(" "), {
-        cwd: fixtureUrl,
-        env,
-        shell: true,
-      });
-
-      let stdoutOutput = "";
-      tstyche.stdout.setEncoding("utf8");
-      tstyche.stdout.on("data", (data) => {
-        stdoutOutput += data;
-      });
-
-      let stderrOutput = "";
-      tstyche.stderr.setEncoding("utf8");
-      tstyche.stderr.on("data", (data) => {
-        stderrOutput += data;
-      });
-
-      tstyche.on("error", (error) => {
-        reject(error);
-      });
-
-      tstyche.on("close", (exitCode) => {
-        // TODO retries avoid flakiness using TypeScript 7.0, can be removed in the future
-        if (stderrOutput === "context canceled\n" && attempt < maxRetries) {
-          const delay = baseDelay * 2 ** (attempt - 1);
-          setTimeout(runWithRetry, delay);
-
-          return;
-        }
-
-        resolve({ exitCode, stderr: stderrOutput, stdout: stdoutOutput });
-      });
+    /** @type {NodeJS.ProcessEnv} */
+    const env = {
+      // TODO remove after adding support for TypeScript 7
+      ["TSTYCHE_TYPESCRIPT_SPECIFIER"]: "@typescript/typescript6",
+      ...process.env,
+      ["TSTYCHE_NO_COLOR"]: "true",
+      ["TSTYCHE_STORE_PATH"]: "./.store",
+      ...options.env,
     };
 
-    runWithRetry();
+    const tstyche = spawn(["tstyche", ...args].join(" "), {
+      cwd: fixtureUrl,
+      env,
+      shell: true,
+    });
+
+    let stdoutOutput = "";
+
+    tstyche.stdout.setEncoding("utf8");
+
+    tstyche.stdout.on("data", (data) => {
+      stdoutOutput += data;
+    });
+
+    let stderrOutput = "";
+
+    tstyche.stderr.setEncoding("utf8");
+
+    tstyche.stderr.on("data", (data) => {
+      stderrOutput += data;
+    });
+
+    tstyche.on("error", (error) => {
+      reject(error);
+    });
+
+    tstyche.on("close", (exitCode) => {
+      resolve({ exitCode, stderr: stderrOutput, stdout: stdoutOutput });
+    });
   });
 }
