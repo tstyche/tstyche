61d2a15cb0c3fd41054fe60960d20dc83ead6b2a
mrazauskas <tom@mrazauskas.de>
2026-09-05 09:41:15 +0300

    add more tests
    Signed-off-by: mrazauskas <tom@mrazauskas.de>

--- tests/__fixtures__/api-toBe/__typetests__/infinite-recursion.tst.ts
+++ tests/__fixtures__/api-toBe/__typetests__/infinite-recursion.tst.ts
@@ -8,3 +8,11 @@ export declare class Collection<Value> {
 }
 
 expect<ReadonlyCollection<{}>>().type.not.toBe<ReadonlyCollection<{ x: string }>>();
+expect<ReadonlyCollection<{}>>().type.toBe<ReadonlyCollection<{ x: string }>>(); // fail
+
+type Box<T> = {
+  value: T;
+};
+
+expect<Box<Box<Box<string>>>>().type.not.toBe<Box<Box<Box<number>>>>();
+expect<Box<Box<Box<string>>>>().type.toBe<Box<Box<Box<number>>>>(); // fail
